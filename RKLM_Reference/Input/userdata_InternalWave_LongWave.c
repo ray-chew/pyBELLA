6730d75d7411b9f4ab2a827fa33316863df6e31f
Rupert Klein <rupert.klein@math.fu-berlin.de>
2018-11-26 08:45:35 +0100

    OK, it is working with  N*dt = 4  and  N*dt = 20  (i.e., a time step of 2000 sec) as well.

--- RKLM_Reference/Input/userdata_InternalWave_LongWave.c
+++ RKLM_Reference/Input/userdata_InternalWave_LongWave.c
@@ -129,8 +129,8 @@ void User_Data_init(User_Data* ud) {
     /* time discretization */
     ud->time_integrator        = SI_MIDPT; /* this code version has only one option */
     ud->CFL                    = 0.9; /* 0.45; 0.9; 0.8; */
-    ud->dtfixed0               = 0.05*(12.5/15.0)*0.5*scalefactor*30.0 / ud->t_ref;
-    ud->dtfixed                = 0.05*(12.5/15.0)*0.5*scalefactor*30.0 / ud->t_ref;
+    ud->dtfixed0               = 1.0*(12.5/15.0)*0.5*scalefactor*30.0 / ud->t_ref;
+    ud->dtfixed                = 1.0*(12.5/15.0)*0.5*scalefactor*30.0 / ud->t_ref;
     
     set_time_integrator_parameters(ud);
     
@@ -186,7 +186,7 @@ void User_Data_init(User_Data* ud) {
     ud->write_stdout = ON;
     ud->write_stdout_period = 1;
     ud->write_file = ON;
-    ud->write_file_period = 300;
+    ud->write_file_period = 30;
     ud->file_format = HDF;
     
     ud->n_time_series = 500; /* n_t_s > 0 => store_time_series_entry() called each timestep */
