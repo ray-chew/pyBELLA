eb680571608917f0576cc4c2f2a851ce3c679a5d
Rupert Klein <rupert.klein@math.fu-berlin.de>
2019-02-16 18:01:56 +0100

    Adding files for simple implementation of molecular transport.

---
+++ RKLM_Reference/Physics/New Group/molecular_transport.c
@@ -0,0 +1,9 @@
+//
+//  molecular_transport.c
+//  RKLM_Reference
+//
+//  Created by Klein, Rupert on 16.02.19.
+//  Copyright © 2019 Klein, Rupert. All rights reserved.
+//
+
+#include "molecular_transport.h"
---
+++ RKLM_Reference/Physics/New Group/molecular_transport.h
@@ -0,0 +1,14 @@
+//
+//  molecular_transport.h
+//  RKLM_Reference
+//
+//  Created by Klein, Rupert on 16.02.19.
+//  Copyright © 2019 Klein, Rupert. All rights reserved.
+//
+
+#ifndef molecular_transport_h
+#define molecular_transport_h
+
+#include <stdio.h>
+
+#endif /* molecular_transport_h */
