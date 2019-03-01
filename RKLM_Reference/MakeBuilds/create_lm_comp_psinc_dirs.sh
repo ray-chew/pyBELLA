e1001cac90c94a038b7074decc5b4d017379d8d3
Tommaso Benacchio <tbenacch@gmail.com>
2019-03-01 11:54:23 +0100

    Adding export_fig dir and viridis colormap for access via relative paths.

--- RKLM_Reference/MakeBuilds/create_lm_comp_psinc_dirs.sh
+++ RKLM_Reference/MakeBuilds/create_lm_comp_psinc_dirs.sh
@@ -8,7 +8,7 @@
 mkdir ../../low_Mach_gravity_comp
 mkdir ../../low_Mach_gravity_psinc
 
-for VAR in 'advflux' 'dpdim' 'geopot' 'interface_data' 'p' 'qc' 'rho' 'rhoZp' 'rhs_nodes' 'second_projection_test' 'u' 'Z' 'buoy' 'drhoY' 'Graphics' 'lap_cells' 'p2_c' 'qr' 'rhoe' 'rhs' 'rhs_second' 'Srhs_cells' 'v' 'dp2_c' 'dY' 'hplus_nodes' 'lap_nodes' 'p2_nodes' 'qv' 'rhoY' 'rhs_cells' 'S' 'theta' 'w' 'dp2_nodes' 'fluxes' 'hydrostate' 'omega' 'p_prolongation' 'res' 'rhoZB' 'rhs_fg' 'S2_cells' 'time_series.txt' 'Y' 'psinc'
+for VAR in 'advflux' 'dpdim' 'geopot' 'interface_data' 'p' 'qc' 'rho' 'rhoZp' 'rhs_nodes' 'second_projection_test' 'u' 'Z' 'buoy' 'drhoY' 'Graphics' 'lap_cells' 'p2_c' 'qr' 'rhoe' 'rhs' 'rhs_second' 'Srhs_cells' 'v' 'dp2_c' 'dY' 'hplus_nodes' 'lap_nodes' 'p2_nodes' 'qv' 'rhoY' 'rhs_cells' 'S' 'theta' 'w' 'dp2_nodes' 'fluxes' 'hydrostate' 'omega' 'p_prolongation' 'res' 'rhoZB' 'rhs_fg' 'S2_cells' 'T' 'dT' 'Y' 'psinc'
 do
   mkdir ../../low_Mach_gravity_comp/$VAR
   mkdir ../../low_Mach_gravity_psinc/$VAR
--- RKLM_Reference/MakeBuilds/run_suite.sh
+++ RKLM_Reference/MakeBuilds/run_suite.sh
@@ -4,8 +4,12 @@
 # for the paper "A semi-implicit numerical model for small-to-planetary scale atmospheric
 # dynamics"
 
+# Straka
 #for TESTCASE in 'Straka_3D_400m' 'Straka_3D_200m' 'Straka_3D_100m' 'Straka_3D_50m'
-for TESTCASE in 'TravellingVortex_3D_192' 'InternalWave_NH' 'InternalWave_H' 'InternalWave_P' 'Straka_3D_50m' 'InternalWave_Baldauf'
+# SK94
+for TESTCASE in 'InternalWave_NH' 'InternalWave_H' 'InternalWave_P'
+# Tests in paper
+#for TESTCASE in 'TravellingVortex_3D_192' 'InternalWave_NH' 'InternalWave_H' 'InternalWave_P' 'Straka_3D_50m' 'InternalWave_Baldauf'
 do
   ./run_test.sh $TESTCASE 
 done
