18953bbaa1706d3672b43a02d018cbd4502e7600
Ray Chew <ray.chew@fu-berlin.de>
2019-04-29 18:03:06 +0200

    added MPV

--- RKLM_Python/physics/low_mach/mpv.py
+++ RKLM_Python/physics/low_mach/mpv.py
@@ -1,3 +1,26 @@
+import numpy as np
+from management.variable import Var
+
 class MPV(object):
     def __init__(self,elem,node,ud):
-        None
+        self.nc = elem.nc
+        self.nn = node.nc
+
+        self.p0 = 1.0
+        self.p00 = 1.0
+
+        self.p2_cells = np.zeros((self.nc))
+        self.dp2_cells = np.zeros((self.nc))
+        self.p2_nodes = np.zeros((self.nn))
+        self.p2_nodes0 = np.zeros((self.nn))
+        self.dp2_nodes = np.zeros((self.nn))
+
+        self.rhs = np.zeros((self.nc))
+        self.diaginv = np.zeros((self.nc))
+        self.wcenter = np.zeros((self.nn))
+        self.wgrav = np.zeros((self.nn))
+        self.wplus = np.zeros((elem.ndim,self.nn))
+
+        self.HydroState = Var(self.nc,ud)
+        self.HydroState_n = Var(self.nn,ud)
+
