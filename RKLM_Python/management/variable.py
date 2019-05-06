ff6d58b225098c92cc13f7596c76493d39e89060
Ray Chew <ray.chew@fu-berlin.de>
2019-05-06 14:07:18 +0200

    added periodic and wall BCs

--- RKLM_Python/management/variable.py
+++ RKLM_Python/management/variable.py
@@ -11,6 +11,31 @@ class Vars(object):
         self.rhoY = np.zeros((size))
         self.rhoX = np.zeros((ud.nspec, size))
 
+    # method written for 2D
+    def set_periodic(self,elem):
+        igx = elem.igx
+
+        # can be rewritten better in terms of a function.
+        self.rho[:,:igx], self.rho[:,-igx:] = self.rho[:,-igx-igx:-igx], self.rho[:,igx+1:igx+igx+1].copy()
+        self.rhou[:,:igx], self.rhou[:,-igx:] = self.rhou[:,-igx-igx:-igx], self.rhou[:,igx+1:igx+igx+1].copy()
+        self.rhov[:,:igx], self.rhov[:,-igx:] = self.rhov[:,-igx-igx:-igx], self.rhov[:,igx+1:igx+igx+1].copy()
+        self.rhow[:,:igx], self.rhow[:,-igx:] = self.rhow[:,-igx-igx:-igx], self.rhow[:,igx+1:igx+igx+1].copy()
+        self.rhoe[:,:igx], self.rhoe[:,-igx:] = self.rhoe[:,-igx-igx:-igx], self.rhoe[:,igx+1:igx+igx+1].copy()
+        self.rhoY[:,:igx], self.rhoY[:,-igx:] = self.rhoY[:,-igx-igx:-igx], self.rhoY[:,igx+1:igx+igx+1].copy()
+        self.rhoX[:,:,0:igx], self.rhoX[:,:,-igx:] = self.rhoX[:,:,-igx-igx:-igx], self.rhoX[:,:,igx+1:igx+igx+1].copy()
+
+    def set_wall(self,elem):
+        igx = elem.igx
+
+        # again, can be written in a swap_columns function and a for loop... but first this.
+        self.rho[:,:igx], self.rho[:,-igx:] = self.rho[:,igx:igx+igx][:,::-1], self.rho[:,-igx-igx:-igx][:,::-1].copy()
+        self.rhou[:,:igx], self.rhou[:,-igx:] = -1.*self.rhou[:,igx:igx+igx][:,::-1], -1.*self.rhou[:,-igx-igx:-igx][:,::-1].copy()
+        self.rhov[:,:igx], self.rhov[:,-igx:] = self.rhov[:,igx:igx+igx][:,::-1], self.rhov[:,-igx-igx:-igx][:,::-1].copy()
+        self.rhow[:,:igx], self.rhow[:,-igx:] = self.rhow[:,igx:igx+igx][:,::-1], self.rhow[:,-igx-igx:-igx][:,::-1].copy()
+        self.rhoe[:,:igx], self.rhoe[:,-igx:] = self.rhoe[:,igx:igx+igx][:,::-1], self.rhoe[:,-igx-igx:-igx][:,::-1].copy()
+        self.rhoY[:,:igx], self.rhoY[:,-igx:] = self.rhoY[:,igx:igx+igx][:,::-1], self.rhoY[:,-igx-igx:-igx][:,::-1].copy()
+
+
 # equivalent to ConsVars_new
 class StatesSmall(Vars):
     def __init__(self,size,ud):
