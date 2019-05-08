28d65deac2ab38eeea53cf305f650a1f5c7107f8
Ray Chew <ray.chew@fu-berlin.de>
2019-05-08 17:10:25 +0200

    in the process of pythonizing stuff...

--- RKLM_Python/management/variable.py
+++ RKLM_Python/management/variable.py
@@ -9,31 +9,55 @@ class Vars(object):
         self.rhow = np.zeros((size))
         self.rhoe = np.zeros((size))
         self.rhoY = np.zeros((size))
-        self.rhoX = np.zeros((ud.nspec, size))
+        self.rhoX = np.zeros(([ud.nspec] + list(size)))
+
+    # will be a better way of doing this
+    def squeezer(self):
+        for key, value in vars(self).items():
+            setattr(self,key,value.squeeze())
 
     # method written for 2D
-    def set_periodic(self,elem):
-        igx = elem.igx
+    def set_periodic(self,elem,axs):
 
-        # can be rewritten better in terms of a function.
-        self.rho[:,:igx], self.rho[:,-igx:] = self.rho[:,-igx-igx:-igx], self.rho[:,igx+1:igx+igx+1].copy()
-        self.rhou[:,:igx], self.rhou[:,-igx:] = self.rhou[:,-igx-igx:-igx], self.rhou[:,igx+1:igx+igx+1].copy()
-        self.rhov[:,:igx], self.rhov[:,-igx:] = self.rhov[:,-igx-igx:-igx], self.rhov[:,igx+1:igx+igx+1].copy()
-        self.rhow[:,:igx], self.rhow[:,-igx:] = self.rhow[:,-igx-igx:-igx], self.rhow[:,igx+1:igx+igx+1].copy()
-        self.rhoe[:,:igx], self.rhoe[:,-igx:] = self.rhoe[:,-igx-igx:-igx], self.rhoe[:,igx+1:igx+igx+1].copy()
-        self.rhoY[:,:igx], self.rhoY[:,-igx:] = self.rhoY[:,-igx-igx:-igx], self.rhoY[:,igx+1:igx+igx+1].copy()
-        self.rhoX[:,:,0:igx], self.rhoX[:,:,-igx:] = self.rhoX[:,:,-igx-igx:-igx], self.rhoX[:,:,igx+1:igx+igx+1].copy()
+        # slice_along = {
+        #     '0' : 1,
+        #     '1' : 0,
+        #     '2' : 2,
+        # }
+        # axs = slice_along[axs]
+
+        
+        # how to loop over attributes of a class?
+        # possible to flip the arrays and use ellipses
+
+        min_ghost = elem.idx_ghost_min[axs]
+        max_ghost = elem.idx_ghost_max[axs]
+        min_inner = elem.idx_inner_min[axs]
+        max_inner = elem.idx_inner_max[axs]
+
+        self.rho[min_ghost], self.rho[max_ghost] = self.rho[max_inner], self.rho[min_inner].copy()
+        self.rhou[min_ghost], self.rhou[max_ghost] = self.rhou[max_inner], self.rhou[min_inner].copy()
+        self.rhov[min_ghost], self.rhov[max_ghost] = self.rhov[max_inner], self.rhov[min_inner].copy()
+        self.rhow[min_ghost], self.rhow[max_ghost] = self.rhow[max_inner], self.rhow[min_inner].copy()
+        self.rhoe[min_ghost], self.rhoe[max_ghost] = self.rhoe[max_inner], self.rhoe[min_inner].copy()
+        self.rhoY[min_ghost], self.rhoY[max_ghost] = self.rhoY[max_inner], self.rhoY[min_inner].copy()
+        self.rhoX[:,:igx], self.rhoX[:,:,-igx:] = self.rhoX[:,max_inner], self.rhoX[:,min_inner].copy()
 
     def set_wall(self,elem):
         igx = elem.igx
 
+        min_ghost = elem.idx(axs,0,igx)
+        max_ghost = elem.idx(axs,-igx,)
+        min_inner = elem.idx(axs,igx,igx+igx+1)
+        max_inner = elem.idx(axs,-igx-igx,-igx)
+
         # again, can be written in a swap_columns function and a for loop... but first this.
-        self.rho[:,:igx], self.rho[:,-igx:] = self.rho[:,igx:igx+igx][:,::-1], self.rho[:,-igx-igx:-igx][:,::-1].copy()
-        self.rhou[:,:igx], self.rhou[:,-igx:] = -1.*self.rhou[:,igx:igx+igx][:,::-1], -1.*self.rhou[:,-igx-igx:-igx][:,::-1].copy()
-        self.rhov[:,:igx], self.rhov[:,-igx:] = self.rhov[:,igx:igx+igx][:,::-1], self.rhov[:,-igx-igx:-igx][:,::-1].copy()
-        self.rhow[:,:igx], self.rhow[:,-igx:] = self.rhow[:,igx:igx+igx][:,::-1], self.rhow[:,-igx-igx:-igx][:,::-1].copy()
-        self.rhoe[:,:igx], self.rhoe[:,-igx:] = self.rhoe[:,igx:igx+igx][:,::-1], self.rhoe[:,-igx-igx:-igx][:,::-1].copy()
-        self.rhoY[:,:igx], self.rhoY[:,-igx:] = self.rhoY[:,igx:igx+igx][:,::-1], self.rhoY[:,-igx-igx:-igx][:,::-1].copy()
+        self.rho[min_ghost], self.rho[max_ghost] = self.rho[min_inner][:,::-1], self.rho[max_inner][:,::-1].copy()
+        self.rhou[min_ghost], self.rhou[max_ghost] = -1.*self.rhou[min_inner][:,::-1], -1.*self.rhou[max_inner][:,::-1].copy()
+        self.rhov[min_ghost], self.rhov[max_ghost] = self.rhov[min_inner][:,::-1], self.rhov[max_inner][:,::-1].copy()
+        self.rhow[min_ghost], self.rhow[max_ghost] = self.rhow[min_inner][:,::-1], self.rhow[max_inner][:,::-1].copy()
+        self.rhoe[min_ghost], self.rhoe[max_ghost] = self.rhoe[min_inner][:,::-1], self.rhoe[max_inner][:,::-1].copy()
+        self.rhoY[min_ghost], self.rhoY[max_ghost] = self.rhoY[min_inner][:,::-1], self.rhoY[max_inner][:,::-1].copy()
 
 
 # equivalent to ConsVars_new
@@ -49,7 +73,7 @@ class StatesSmall(Vars):
         self.entro = np.zeros((size))
         self.H = np.zeros((size))
         self.Y = np.zeros((size))
-        self.X = np.zeros((ud.nspec, size))
+        self.X = np.zeros(([ud.nspec] + list(size)))
 
         self.p0 = np.zeros((size))
         self.p20 = np.zeros((size))
@@ -58,10 +82,14 @@ class StatesSmall(Vars):
         self.S10 = np.zeros((size))
         self.Y0 = np.zeros((size))
 
+        self.squeezer()
+
 class States(StatesSmall):
     def __init__(self,size,ud):
         super().__init__(size,ud)
         self.pi0 = np.zeros((size))
         self.rhoY0 = np.zeros((size))
+
+        self.squeezer()
 
         
--- RKLM_Python/physics/low_mach/mpv.py
+++ RKLM_Python/physics/low_mach/mpv.py
@@ -6,20 +6,23 @@ class MPV(object):
         self.nc = elem.nc
         self.nn = node.nc
 
+        self.sc = elem.sc
+        self.sn = node.sc
+
         self.p0 = 1.0
         self.p00 = 1.0
 
-        self.p2_cells = np.zeros((self.nc))
-        self.dp2_cells = np.zeros((self.nc))
-        self.p2_nodes = np.zeros((self.nn))
-        self.p2_nodes0 = np.zeros((self.nn))
-        self.dp2_nodes = np.zeros((self.nn))
+        self.p2_cells = np.zeros((self.sc))
+        self.dp2_cells = np.zeros((self.sc))
+        self.p2_nodes = np.zeros((self.sn))
+        self.p2_nodes0 = np.zeros((self.sn))
+        self.dp2_nodes = np.zeros((self.sn))
 
-        self.rhs = np.zeros((self.nc))
-        self.diaginv = np.zeros((self.nc))
-        self.wcenter = np.zeros((self.nn))
-        self.wgrav = np.zeros((self.nn))
-        self.wplus = np.zeros((elem.ndim,self.nn))
+        self.rhs = np.zeros((self.sc))
+        self.diaginv = np.zeros((self.sc))
+        self.wcenter = np.zeros((self.sn))
+        self.wgrav = np.zeros((self.sn))
+        self.wplus = np.zeros(([elem.ndim]+list(self.sn)))
 
-        self.HydroState = States(self.nc,ud)
-        self.HydroState_n = States(self.nn,ud)
+        self.HydroState = States(self.sc,ud)
+        self.HydroState_n = States(self.sn,ud)
