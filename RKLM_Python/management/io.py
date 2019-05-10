770735e0ce2ff960f77d2bfdc98e79a473bdbad2
Ray Chew <ray.chew@fu-berlin.de>
2019-05-11 00:33:02 +0200

    added all test arrays (corresponding to c-output)

--- RKLM_Python/management/io.py
+++ RKLM_Python/management/io.py
@@ -1,5 +1,6 @@
 # input/output
 import h5py
+import numpy as np
 
 class io(object):
     def __init__(self):
@@ -41,7 +42,7 @@ class io(object):
                 file.create_group(path,track_order=True)
         file.close()
 
-    def write_all(self,Sol,mpv,elem,node,th,name):
+    def write_all(self,Sol,mpv,elem,node,th,name,ud):
         # rho
         self.populate(name,'rho',Sol.rho)
         # rhoe
@@ -60,12 +61,60 @@ class io(object):
         self.populate(name,'p2_nodes',mpv.p2_nodes)
         # p2_cells
         self.populate(name,'p2_cells',mpv.p2_cells)
+        # dp2_cells
+        self.populate(name,'dp2_cells',mpv.dp2_cells)
 
         # pressure
         self.populate(name,'p',Sol.rhoY**th.gamm)
+        # pressure difference
+        self.populate(name,'dpdim', self.dpress_dim(mpv,ud,th))
 
-        # velocity (u)
+        # velocity (u,v,w)
         self.populate(name,'u',Sol.rhou / Sol.rho)
+        self.populate(name,'v',Sol.rhov / Sol.rho)
+        self.populate(name,'w',Sol.rhow / Sol.rho)
+
+        # vorticity in (x,z)
+        self.populate(name,'vortz', self.vortz(Sol,elem,node))
+
+        # temperature
+        self.populate(name,'T', Sol.rhoY**th.gamm / Sol.rho)
+        # temperature difference
+        self.populate(name,'dT', Sol.rhoY**th.gamm / Sol.rho - mpv.HydroState.p0[0,:] / mpv.HydroState.rho0[0,:])
+
+        # species mass fraction(?)
+        self.populate(name,'Y', Sol.rhoY / Sol.rho)
+        # species mass fraction perturbation
+        self.populate(name,'dY', Sol.rhoY / Sol.rho - ud.stratification(elem.y))
+
+    def vortz(self,Sol,elem,node):
+        # 2d-case
+        igs = elem.igs
+        dx = elem.dx
+        dy = elem.dy
+        inner_domain_rho = Sol.rho[igs[0]-1:-igs[0]+1, igs[1]-1:-igs[1]+1]
+        inner_domain_rhou = Sol.rho[igs[0]-1:-igs[0]+1, igs[1]-1:-igs[1]+1]
+        inner_domain_rhov = Sol.rho[igs[0]-1:-igs[0]+1, igs[1]-1:-igs[1]+1]
+
+        top_left_idx     = (slice(0,-1)    , slice(0,-1))
+        top_right_idx    = (slice(0,-1)    , slice(1, None))
+        bottom_left_idx  = (slice(1, None) , slice(0,-1))
+        bottom_right_idx = (slice(1, None) , slice(1,None))
+
+        dvdx = 0.5 * ((inner_domain_rhov[bottom_right_idx] / inner_domain_rho[bottom_right_idx] - inner_domain_rhov[bottom_left_idx] / inner_domain_rho[bottom_left_idx]) + (inner_domain_rhov[top_right_idx] / inner_domain_rho[top_right_idx] - inner_domain_rhov[top_left_idx] / inner_domain_rho[top_left_idx])) / dx
+
+        dudy = 0.5 * ((inner_domain_rhou[bottom_right_idx] / inner_domain_rho[bottom_right_idx] - inner_domain_rhou[bottom_left_idx] / inner_domain_rho[bottom_left_idx]) + inner_domain_rhou[top_right_idx] / inner_domain_rho[top_right_idx] - inner_domain_rhou[top_left_idx] / inner_domain_rho[top_left_idx]) / dy
+
+        vortz = np.zeros((node.sc)).squeeze()
+
+        vortz[igs[0]:-igs[0], igs[1]:-igs[1]] = dvdx - dudy
+        return vortz
+
+    def dpress_dim(self,mpv,ud,th):
+        p = (th.Gamma * ud.Msq * mpv.p2_cells)**th.Gammainv
+        return (p - mpv.HydroState.p0[0,:]) * ud.p_ref
+
+
 
     def populate(self,name,path,data,options=None):
         # name is the simulation time of the output array
