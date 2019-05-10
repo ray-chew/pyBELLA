a67470ed709510e823cf7f4a77251be7a40dfcc0
Ray Chew <ray.chew@fu-berlin.de>
2019-05-10 12:15:40 +0200

    enumerator now reflects np.pad keywords

--- RKLM_Python/inputs/enum_bdry.py
+++ RKLM_Python/inputs/enum_bdry.py
@@ -2,10 +2,10 @@ from enum import Enum # ! Version > Python 3.4
 
 class BdryType(Enum):
     TUNIX = 0
-    WALL = 1
+    WALL = 'symmetric'
     INFLOW = 2
     OUTFLOW = 3
-    PERIODIC = 4
+    PERIODIC = 'wrap'
     NEUMANN = 5
     DIRICHLET = 6
     OPEN = 7
