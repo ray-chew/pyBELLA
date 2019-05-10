2fd431275eb65eb30c0a60e142c2cc8fadef1fa3
Ray Chew <ray.chew@fu-berlin.de>
2019-05-10 12:18:10 +0200

    debugged mpv

--- RKLM_Python/physics/low_mach/mpv.py
+++ RKLM_Python/physics/low_mach/mpv.py
@@ -3,26 +3,30 @@ from management.variable import States
 
 class MPV(object):
     def __init__(self,elem,node,ud):
-        self.nc = elem.nc
-        self.nn = node.nc
-
-        self.sc = elem.sc
-        self.sn = node.sc
-
+        sc = elem.sc
+        sn = node.sc
+        
         self.p0 = 1.0
         self.p00 = 1.0
 
-        self.p2_cells = np.zeros((self.sc))
-        self.dp2_cells = np.zeros((self.sc))
-        self.p2_nodes = np.zeros((self.sn))
-        self.p2_nodes0 = np.zeros((self.sn))
-        self.dp2_nodes = np.zeros((self.sn))
+        self.p2_cells = np.zeros((sc))
+        self.dp2_cells = np.zeros((sc))
+        self.p2_nodes = np.zeros((sn))
+        self.p2_nodes0 = np.zeros((sn))
+        self.dp2_nodes = np.zeros((sn))
 
-        self.rhs = np.zeros((self.sc))
-        self.diaginv = np.zeros((self.sc))
-        self.wcenter = np.zeros((self.sn))
-        self.wgrav = np.zeros((self.sn))
-        self.wplus = np.zeros(([elem.ndim]+list(self.sn)))
+        self.rhs = np.zeros((sc))
+        self.diaginv = np.zeros((sc))
+        self.wcenter = np.zeros((sn))
+        self.wgrav = np.zeros((sn))
+        self.wplus = np.zeros(([elem.ndim]+list(sn)))
 
-        self.HydroState = States(self.sc,ud)
-        self.HydroState_n = States(self.sn,ud)
+        self.HydroState = States(sc,ud)
+        self.HydroState_n = States(sn,ud)
+
+        self.squeezer()
+
+    def squeezer(self):
+        for key, value in vars(self).items():
+            if type(value) == np.ndarray:
+                setattr(self,key,value.squeeze())
