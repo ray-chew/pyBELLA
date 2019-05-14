73df87ac19069b7c66c81902fb8d59c449ffaf8b
Ray Chew <ray.chew@fu-berlin.de>
2019-05-14 17:04:15 +0200

    corrected bc in hydrostatic; need to tidy this up eventually!

--- RKLM_Python/physics/hydrostatics/hydrostatics.py
+++ RKLM_Python/physics/hydrostatics/hydrostatics.py
@@ -105,22 +105,17 @@ def hydrostatic_state(mpv, elem, node, th, ud):
     mpv.HydroState_n.p0[0,igy] = p0
     mpv.HydroState_n.p20[0,igy] = pi0 / ud.Msq
 
-    y_p = elem.y[igy-1]
-    S_p = 1.0 / ud.stratification(y_p)
-    S_integral_p = -0.5 * elem.dy * 0.5 * (S_p + 1.0 / ud.stratification(0.0))
-
-    yn_p = node.y[igy-1]
-
-    Sn_p = 1.0 / ud.stratification(elem.y[igy-1])
-    Sn_integral_p = -node.dy * Sn_p
-
     ###########################
+    y_p = elem.y[igy-1]
     y_m = np.copy(y_p)
-    y_p = y_m - elem.dy
+
+    y_p = elem.y[:igy]
+
     S_p = 1.0 / ud.stratification(y_p)
-    # S_integral_p -= np.arange(igy)[::-1] * elem.dy * 0.5 * (S_m + S_p)
-    S_integral_p -= np.arange(igy) * elem.dy
-    S_integral_p = S_integral_p[::-1]
+    S_m = [S_p[1], 1./ud.stratification(0.0)]
+    S_integral_p = -elem.dy * 0.5 * (S_p + S_m)
+    S_integral_p[1] *= 0.5
+    S_integral_p[0] += S_integral_p[1]
 
     pi_hydro = pi0 - Gamma * g * S_integral_p
     p_hydro = pi_hydro**Gamma_inv
@@ -134,9 +129,12 @@ def hydrostatic_state(mpv, elem, node, th, ud):
     mpv.HydroState.S10[0,:igy] = 0.0
     mpv.HydroState.Y0[0,:igy] = 1.0 / S_p
 
+    yn_p = node.y[igy-1]
     yn_m = np.copy(yn_p)
     yn_p = yn_m - elem.dy
     # Sn_m = np.copy(Sn_p)
+    Sn_p = 1.0 / ud.stratification(elem.y[igy-1])
+    Sn_integral_p = -node.dy * Sn_p
     Sn_p = 1.0 / ud.stratification(0.5*(yn_p + yn_m))
     Sn_integral_p -= np.arange(igy) * elem.dy * Sn_p
     Sn_integral_p = Sn_integral_p[::-1]
