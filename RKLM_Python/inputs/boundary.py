a0af301c063fe652071485a62a2d62f0706c7f12
Ray Chew <ray.chew@fu-berlin.de>
2019-05-14 16:22:19 +0200

    completed gravity BCs

--- RKLM_Python/inputs/boundary.py
+++ RKLM_Python/inputs/boundary.py
@@ -98,17 +98,97 @@ def set_wall_rhoYflux(bdry, Sol0, mpv, elem, ud):
 def wall_rhoYflux(x,z,wind_speed_x,wind_speed_z,rhoY0,ud):
     return slanted_wall_slope(x,ud) * wind_speed_x * rhoY0
 
-def set_explicit_boundary_data(Sol, elem, ud):
+def set_explicit_boundary_data(Sol, elem, ud, th, mpv):
     igs = elem.igs
     ndim = elem.ndim
 
     for dim in range(ndim):
         ghost_padding, idx = get_ghost_padding(ndim,dim,igs)
-        if ud.bdry_type[dim] == BdryType.PERIODIC:
-            Sol.set_boundary(ghost_padding,'wrap',idx)
+        if ud.gravity_strength[dim] == 0.0:
+            if ud.bdry_type[dim] == BdryType.PERIODIC:
+                set_boundary(Sol,ghost_padding,'wrap',idx)
+            else:
+                set_boundary(Sol,ghost_padding,'symmetric',idx)
         else:
-            Sol.set_boundary(ghost_padding,'symmetric',idx)
+            # recursive updating of array - for loop cannot be avoided....?
+            # assumption: gravity always acts in the y-axis
+            direction = -1.
+            offset = 0
 
+            g = ud.gravity_strength[1]
+
+            for side in ghost_padding[1]:
+                direction *= -1
+                for current_idx in np.arange(side)[::-1]:
+
+                    nlast, nsource, nimage = get_gravity_padding(ndim,current_idx,direction,offset,elem)
+
+                    Y_last = Sol.rhoY[nlast] / Sol.rho[nlast]
+                    u = Sol.rhou[nsource] / Sol.rho[nsource]
+                    w = Sol.rhow[nsource] / Sol.rho[nsource]
+                    X = Sol.rhoX[nsource] / Sol.rho[nsource]
+
+                    rhoYv_image = Sol.rhov[nsource] * Sol.rhoY[nsource] / Sol.rho[nsource]
+
+                    S = 1. / ud.stratification(elem.y[nimage[1]])
+
+                    dpi = direction*(th.Gamma*g) * 0.5 * elem.dy * (1.0 / Y_last + S)
+                    rhoY = ((Sol.rhoY[nlast]**th.gm1) + dpi)**th.gm1inv if ud.is_compressible == 1 else mpv.HydroState.rhoY0[nimage]
+
+                    rho = rhoY * S
+                    p = rhoY**th.gamm
+                    if np.sign(direction) == 1:
+                        v = rhoYv_image / rhoY
+                    else:
+                        v = -Sol.rhov[nsource] / Sol.rho[nsource]
+
+                    Sol.rho[nimage] = rho
+                    Sol.rhou[nimage] = rho*u
+                    Sol.rhov[nimage] = rho*v
+                    Sol.rhow[nimage] = rho*w
+                    Sol.rhoe[nimage] = ud.rhoe(rho, u, v, w, p, ud, th)
+                    Sol.rhoY[nimage] = rhoY
+                    Sol.rhoX[nimage] = rho * X
+
+                offset += 1
+
+def set_boundary(Sol,pads,btype,idx):
+    Sol.rho[...] = np.pad(Sol.rho[idx],pads,btype)
+    if btype == 'symmetric':
+        Sol.rhou[...] = np.pad(Sol.rhou[idx],pads,negative_symmetric)
+    else:
+        Sol.rhou[...] = np.pad(Sol.rhou[idx],pads,btype)
+    Sol.rhov[...] = np.pad(Sol.rhov[idx],pads,btype)
+    Sol.rhow[...] = np.pad(Sol.rhow[idx],pads,btype)
+    Sol.rhoe[...] = np.pad(Sol.rhoe[idx],pads,btype)
+    Sol.rhoY[...] = np.pad(Sol.rhoY[idx],pads,btype)
+    Sol.rhoX[...] = np.pad(Sol.rhoX[idx],pads,btype)
+
+def negative_symmetric(vector,pad_width,iaxis,kwargs=None):
+    if pad_width[1] > 0:
+        sign = 1
+        vector[:pad_width[0]] = sign * vector[pad_width[0]:2*pad_width[0]][::-1]
+        vector[-pad_width[1]:] = sign * vector[-2*pad_width[1]:-pad_width[1]][::-1]
+        return vector
+    else: # axis must have length > 0.
+        return vector
+
+def get_gravity_padding(ndim,cur_idx,direction,offset,elem):
+    cur_i = np.copy(cur_idx)
+    cur_idx += offset * ((elem.icy - 1) - 2*cur_idx)   
+    gravity_padding = [(slice(None))] * ndim
+    y_axs = ndim - 1
+
+    nlast = np.copy(gravity_padding)
+    nlast[y_axs] = int(cur_idx + direction)
+
+    nsource = np.copy(gravity_padding)
+    nsource[y_axs] = int(offset*(elem.icy) + direction * (2 * elem.igy - (1 - offset) - cur_i))
+
+    nimage = np.copy(gravity_padding)
+    nimage[y_axs] = int(cur_idx)
+    # print(tuple(nlast))
+    return tuple(nlast), tuple(nsource), tuple(nimage)
 
 def get_ghost_padding(ndim,dim,igs):
     ghost_padding = [(0,0)] * ndim
