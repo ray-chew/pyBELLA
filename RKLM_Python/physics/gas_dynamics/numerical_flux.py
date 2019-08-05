81c04830f9cab942fbd53a3c62c3f117ea2e0a3e
Ray Chew <ray.chew@fu-berlin.de>
2019-08-05 20:22:15 +0200

    completed function recompute_advective_flux

--- RKLM_Python/physics/gas_dynamics/numerical_flux.py
+++ RKLM_Python/physics/gas_dynamics/numerical_flux.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import signal
 
-def recompute_advective_fluxes(flux, Sol, elem, dt):
+def recompute_advective_fluxes(flux, Sol):
     ################################################
     # 2D case for now - generalise in future
     ################################################
@@ -9,39 +9,22 @@ def recompute_advective_fluxes(flux, Sol, elem, dt):
     left_idx = (slice(None),slice(0,-2))
     right_idx = (slice(None),slice(1,-1))
 
-    # left_idx = (slice(None),slice(1,-1))
-    # right_idx = (slice(None),slice(2,None))
-
     top_idx = (slice(0,-2),)
     bottom_idx = (slice(1,-1),)
-
-    umid_idx = (slice(1,-1),)
-    vmid_idx = (slice(None),slice(1,-1))
     
-    BB = np.arange(25).reshape(5,5)
-    print(BB)
-    # print(BB[left_idx])
-    # print(BB[right_idx])
+    rhoYu = Sol.rhoY * Sol.rhou.T / Sol.rho
 
-    # print(BB[top_idx])
-    # print(BB[bottom_idx])
-    
-    rhoYu = Sol.rhoY * Sol.rhou / Sol.rho
-    rhoYu_interface = 0.5 * (rhoYu[left_idx] + rhoYu[right_idx])
-    print(rhoYu_interface.shape)
-    kernel_u = np.array([[1.],[2.],[1.]])
+    # idx = 901
+    # print("rhoYu_cm = ", rhoYu.flatten()[idx-52])
+    # print("rhoYu_mm = ", rhoYu.flatten()[idx-52-1])
+    # print("rhoYu_cc = ", rhoYu.flatten()[idx])
+    # print("rhoYu_mc = ", rhoYu.flatten()[idx-1])
+    # print("rhoYu_cp = ", rhoYu.flatten()[idx+52])
+    # print("rhoYu_mp = ", rhoYu.flatten()[idx+52-1])
 
-    print(flux[0].rhoY.shape)
-    BBp = signal.convolve2d(BB,kernel_u,mode='valid')
-    # print(BBp)
-    print(signal.convolve2d(rhoYu_interface,kernel_u, mode='valid').shape)
-    flux[0].rhoY[1:-2,1:-1] = signal.convolve2d(rhoYu_interface,kernel_u, mode='valid') / kernel_u.sum()
+    kernel_u = np.array([[0.5, 0.5],[1., 1.],[0.5, 0.5]])
+    flux[0].rhoY[1:-2,1:-1] = signal.convolve2d(rhoYu[:,:-1], kernel_u, mode='valid').T / kernel_u.sum()
 
-    rhoYv = Sol.rhoY * Sol.rhov / Sol.rho
-    rhoYv_interface = 0.5 * (rhoYv[top_idx] + rhoYv[bottom_idx])
-    print(rhoYv_interface.shape)
-
-    kernel_v = np.array([[1.,2.,1.]])
-    flux[1].rhoY[1:-1,1:-2] = signal.convolve2d(rhoYv_interface,kernel_v, mode='valid') / kernel_v.sum()
-
-    # print(flux[0].rhoY)
+    rhoYv = Sol.rhoY * Sol.rhov.T / Sol.rho
+    kernel_v = np.array([[0.5,1.,0.5],[0.5,1.,0.5]])
+    flux[1].rhoY[1:-1,1:-2] = signal.convolve2d(rhoYv[:-1,:],kernel_v, mode='valid').T / kernel_v.sum()
