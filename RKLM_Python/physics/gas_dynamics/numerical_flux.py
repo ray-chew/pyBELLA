fixed dimensionality issues with recovery and numerical flux

RKLM_Python/physics/gas_dynamics/numerical_flux.py
@@ -20,11 +20,11 @@ def recompute_advective_fluxes(flux, Sol):
     # print("rhoYu_mp = ", rhoYu.flatten()[idx+52-1])
 
     kernel_u = np.array([[0.5, 0.5],[1., 1.],[0.5, 0.5]])
-    flux[0].rhoY[1:-2,1:-1] = signal.convolve2d(rhoYu[:,:-1], kernel_u, mode='valid').T / kernel_u.sum()
+    flux[0].rhoY[1:-1,1:-1] = signal.convolve2d(rhoYu, kernel_u, mode='valid') / kernel_u.sum()
 
     rhoYv = Sol.rhoY * Sol.rhov.T / Sol.rho
     kernel_v = np.array([[0.5,1.,0.5],[0.5,1.,0.5]])
-    flux[1].rhoY[1:-1,1:-2] = signal.convolve2d(rhoYv[:-1,:], kernel_v, mode='valid').T / kernel_v.sum()
+    flux[1].rhoY[1:-1,1:-1] = signal.convolve2d(rhoYv, kernel_v, mode='valid').T / kernel_v.sum()
 
 
 def hll_solver(flux, Lefts, Rights, Sol, lmbda, ud, th):
@@ -32,7 +32,7 @@ def hll_solver(flux, Lefts, Rights, Sol, lmbda, ud, th):
     # flux: index 1 to end = Left[inner_idx]: index 0 to -1 = Right[inner_idx]: index 1 to end
     inner_idx = (slice(1,-1),slice(1,-1))
     # flux_inner_idx = (slice(1,-2),slice(1,-1))
-    flux_inner_idx = flux.flux_inner_idx
+    # flux_inner_idx = (slice(None),slice(0,-1))
     left_idx = (slice(None),slice(0,-1))
     right_idx = (slice(None),slice(1,None))
     first_col = (slice(None),slice(0))
@@ -41,13 +41,29 @@ def hll_solver(flux, Lefts, Rights, Sol, lmbda, ud, th):
     Rights.primitives(th)
     
     upwind = 0.5 * (1.0 + np.sign(flux.rhoY))
-    
+    # print(upwind.shape)
     # print(upwind.shape)
     # print(Lefts.u[0])
-    upl = upwind / Lefts.Y[inner_idx]
-    upr = (1.0 - upwind) / Rights.Y[inner_idx]
-    print(flux.rhou.shape)
-    
-    flux.rhou[flux_inner_idx][:,1:] = flux.rhoY[right_idx] * (upl[right_idx] * Lefts.u[inner_idx][left_idx] + upr[right_idx] * Rights.u[inner_idx][right_idx])
-    flux.rhou[first_col] = 0.0
-    print(flux.rhou[1])
+    upl = upwind[right_idx] # / Lefts.Y
+    # print(Lefts.Y)
+    upr = (1.0 - upwind[left_idx]) # / Rights.Y
+    # print(flux.rhou.shape)
+    # print((upl[left_idx] * Lefts.u[left_idx]).shape)
+    # print((upr[right_idx] * Rights.u[right_idx]).shape)
+    # print(flux.rhoY[:,1:-1].shape)
+
+    flux.rhou[:,1:-1] = flux.rhoY[:,1:-1] * (upl[left_idx] / Lefts.Y[left_idx] * Lefts.u[left_idx] + upr[right_idx] / Rights.Y[right_idx] * Rights.u[right_idx])
+
+    flux.rho[:,1:-1] = flux.rhoY[:,1:-1] * (upl[left_idx] / Lefts.Y[left_idx] * 1.0 + upr[right_idx] / Rights.Y[right_idx] * 1.0)
+
+    # flux.rhou[first_col] = 0.0
+    # print(flux.rhou[1])
+
+    val, idx = find_nearest(flux.rhou,0.50000002985023706)
+    # print(val, idx)
+    # print(flux.rhou[1])
+def find_nearest(array, value):
+    # print(array.shape)
+    idx = (np.abs(array - value)).argmin()
+    # print(idx)
+    return array.flat[idx], idx

RKLM_Python/physics/gas_dynamics/recovery.py
@@ -12,12 +12,15 @@ def recovery(Sol, flux, lmbda, ud, th, elem):
 
     lefts_idx = (slice(None),slice(0,-1))
     rights_idx = (slice(None),slice(1,None))
+    inner_idx = (slice(1,-1), slice(1,-1))
 
     # inner_idx here are where the interface fluxes are calculated with non-zero values.
-    face_inner_idx = (slice(1,-1), slice(1,-2))
-
+    face_inner_idx = (slice(1,-1), slice(1,-1))
+    # print(flux.rhoY[1])
     u = np.zeros_like(Sol.rhoY)
-    u[face_inner_idx] = 0.5 * (flux.rhoY[lefts_idx] + flux.rhoY[rights_idx]) / Sol.rhoY[face_inner_idx]
+    # print(flux.rhoY.shape)
+    u[inner_idx] = 0.5 * (flux.rhoY[face_inner_idx][lefts_idx] + flux.rhoY[face_inner_idx][rights_idx]) / Sol.rhoY[inner_idx]
+    
 
     Diffs = States(elem.sc,ud)
     Ampls = Characters(elem.sc)