3512d414d276bb6c0cc9e80d2943d3dd7d7d2f17
Ray Chew <ray.chew@fu-berlin.de>
2019-08-12 14:49:31 +0200

    ud values: nonhydrostasy and compressibility (eos.py), acoustic order (mpv.py) added

---
+++ RKLM_Python/physics/gas_dynamics/eos.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def nonhydrostasy(ud,t):
+    if ud.is_nonhydrostatic == 0:
+        return 0.0
+    elif ud.is_nonhydrostatic == 1:
+        return 1,0
+    elif ud.is_nonhydrostatic == -1:
+        a = 12.5
+        b = 1.0 / 24.0
+        c = np.min(1.0, np.max(0.0, b * (t-a)))
+        return c
+    else:
+        assert 0
+
+def compressibility(ud,t):
+    if ud.is_compressible == 0:
+        return 0,0
+    elif ud.is_compressible == 1:
+        return 1.0
+    elif ud.is_compressible == -1:
+        dtloc = 12.0
+        a = 0.0 * dtloc
+        b = 1.0 / dtloc / 20.0
+        tau = np.min(1.0, np.max(0.0 * b * (t-a)))
+        c = 0.5 * (1.0 - np.cos(np.pi * tau))
+        return c
+    else:
+        assert 0
+
