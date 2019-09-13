144225de747777022a3f4e802cf0517d4cdd41ea
Ray Chew <ray.chew@fu-berlin.de>
2019-09-14 00:53:26 +0200

    cleaned up numerical_flux.py

--- RKLM_Python/physics/gas_dynamics/numerical_flux.py
+++ RKLM_Python/physics/gas_dynamics/numerical_flux.py
@@ -63,32 +63,14 @@ def recompute_advective_fluxes(flux, Sol):
     """
     rhoYu = Sol.rhoY * Sol.rhou / Sol.rho
 
-    # keep for debugging purpose... last checked, function is correct
-    # will need to think of a better method to generalise from 2d
-
-    # idx = 874
-    # print("rhoYu_cm = ", rhoYu.flatten()[idx-52])
-    # print("rhoYu_mm = ", rhoYu.flatten()[idx-52-1])
-    # print("rhoYu_cc = ", rhoYu.flatten()[idx])
-    # print("rhoYu_mc = ", rhoYu.flatten()[idx-1])
-    # print("rhoYu_cp = ", rhoYu.flatten()[idx+52])
-    # print("rhoYu_mp = ", rhoYu.flatten()[idx+52-1])
-
     kernel_u = np.array([[0.5, 0.5],[1., 1.],[0.5, 0.5]]).T
     flux[0].rhoY[1:-1,1:-1] = signal.convolve2d(rhoYu, kernel_u, mode='valid').T / kernel_u.sum()
     flux[0].rhoY[:,-2] *= 0.
-    # print(flux[0].rhoY.shape)
-    # flux[0].rhoY[:,-2] = 0.
-    # flux[0].rhoY = flux[0].rhoY.T
-
-    # print("rhoYu = ", flux[0].rhoY.flatten()[idx-5:idx+5])
-    # find_nearest(flux[0].rhoY, 1.2420080775988327)
 
     rhoYv = Sol.rhoY * Sol.rhov / Sol.rho
     kernel_v = np.array([[0.5,1.,0.5],[0.5,1.,0.5]]).T
     flux[1].rhoY[1:-1,1:-1] = signal.convolve2d(rhoYv, kernel_v, mode='valid') /kernel_v.sum()
     flux[1].rhoY[:,-2] *= 0.
-    # flux[1].rhoY = flux[1].rhoY.T
 
 
 def hll_solver(flux, Lefts, Rights, Sol, lmbda, ud, th):
@@ -99,8 +81,6 @@ def hll_solver(flux, Lefts, Rights, Sol, lmbda, ud, th):
 
     Lefts.primitives(th)
     Rights.primitives(th)
-    # print(Lefts.rhoe[0][:10])
-    # print(Lefts.rhoe[0][:10])
     
     upwind = 0.5 * (1.0 + np.sign(flux.rhoY))
     upl = upwind[right_idx]
@@ -112,15 +92,6 @@ def hll_solver(flux, Lefts, Rights, Sol, lmbda, ud, th):
     Hl = Lefts.rhoe[left_idx] + Lefts.p[left_idx]
     Hr = Rights.rhoe[right_idx] + Rights.p[right_idx]
     flux.rhoe[remove_cols_idx] = flux.rhoY[remove_cols_idx] * (upl[left_idx] / Lefts.Y[left_idx] * Hl + upr[right_idx] / Rights.Y[right_idx] * Hr)
-
-    # print("flux.rhoY = ", flux.rhoY[1][:10])
-    # print("upl = ", upl[1][:10] / Lefts.Y[1][:10])
-    # # print("Lefts.Y = ", Lefts.Y[1][:10])
-    # print("Hl = ", Hl[1][:10])
-    # print("upr = ", upr[1][:10] / Rights.Y[1][:10])
-    # # print("Rights.Y = ", Rights.Y[1][:10])
-    # print("Hr = ", Hr[1][:10])
-    # print("flux.rhoe = ", flux.rhoe[1][:10])
 
     flux.rhov[remove_cols_idx] = flux.rhoY[remove_cols_idx] * (upl[left_idx] / Lefts.Y[left_idx] * Lefts.v[left_idx] + upr[right_idx] / Rights.Y[right_idx] * Rights.v[right_idx])
     flux.rhow[remove_cols_idx] = flux.rhoY[remove_cols_idx] * (upl[left_idx] / Lefts.Y[left_idx] * Lefts.w[left_idx] + upr[right_idx] / Rights.Y[right_idx] * Rights.w[right_idx])
