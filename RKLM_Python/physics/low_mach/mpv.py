46a54b27b065e1e96276c10779aab4ea42216cff
Ray Chew <ray.chew@fu-berlin.de>
2019-10-02 18:32:54 +0200

    rising bubble now supports both HY and PI continuous blendings

--- RKLM_Python/physics/low_mach/mpv.py
+++ RKLM_Python/physics/low_mach/mpv.py
@@ -38,7 +38,7 @@ def acoustic_order(ud,t,step):
     elif ud.is_compressible == 1:
         return 2.0
     elif ud.is_compressible == -1:
-        current_transition_step = step - ud.no_of_initial
-        return np.linspace(1.8,2.0,ud.no_of_transition+2)[1:-1][current_transition_step]
+        current_transition_step = step - ud.no_of_pi_initial
+        return np.linspace(1.8,2.0,ud.no_of_pi_transition+2)[1:-1][current_transition_step]
     else:
         assert 0
