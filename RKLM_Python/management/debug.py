6db645e8eb252b5c2964ed1c15513618b828b7c0
Ray Chew <ray.chew@fu-berlin.de>
2019-10-23 16:17:38 +0200

    debug.py: added code ref

--- RKLM_Python/management/debug.py
+++ RKLM_Python/management/debug.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# taken from: https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
 def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     print("val = ", array.flat[idx])
