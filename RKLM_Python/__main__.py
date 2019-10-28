3c190a90c240c2dac2ce59de3c675ad80592dbc8
Ray Chew <ray.chew@fu-berlin.de>
2019-10-28 18:48:11 +0100

    parallelisation of ensemble members for flow solver implemented. but io is inevitably broken.

--- RKLM_Python/__main__.py
+++ RKLM_Python/__main__.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # dependencies of the atmospheric flow solver
-from management.data import data_init, time_update
+from management.data import data_init, time_update, time_update_wrapper
 from management.variable import States, Vars
 from physics.gas_dynamics.thermodynamic import ThemodynamicInit
 from physics.gas_dynamics.eos import nonhydrostasy, compressibility, is_compressible, is_nonhydrostatic
@@ -30,6 +30,7 @@ from copy import deepcopy
 from management.debug import find_nearest
 from time import time
 import h5py
+
 
 debug = False
 np.set_printoptions(precision=18)
@@ -61,41 +62,49 @@ Sol = deepcopy(Sol0)
 dt_factor = 0.5 if ud.initial_impl_Euler == True else 1.0
 
 writer = io(ud)
-writer.write_all(Sol,mpv,elem,node,th,'000_ic')
+# writer.write_all(Sol,mpv,elem,node,th,'000_ic')
 
 tout = ud.tout
 
-##########################################################
-# Data Assimilation part
-N = 1
-da_parameters = da_params(N)
-aprior_error_covar = da_parameters.aprior_error_covar
-# sampler = da_parameters.sampler(aprior_error_covar)
-sampler = da_parameters.sampler_none()
-attributes = da_parameters.attributes
-ens = ensemble()
-ens.initialise_members([Sol,flux,mpv],N)
+if __name__ == '__main__':
+    ##########################################################
+    # Data Assimilation part
+    N = 2
+    da_parameters = da_params(N)
+    aprior_error_covar = da_parameters.aprior_error_covar
+    # sampler = da_parameters.sampler(aprior_error_covar)
+    sampler = da_parameters.sampler_none()
+    attributes = da_parameters.attributes
+    ens = ensemble()
+    ens.initialise_members([Sol,flux,mpv],N)
 
-print(ens.members(ens))
-# ens.ensemble_spreading(sampler,attributes)
-##########################################################
+    # print(ens.members(ens))
+    # ens.ensemble_spreading(sampler,attributes)
 
-tic = time()
+    client = Client(threads_per_worker=2, n_workers=2)
+    ##########################################################
 
-# assert(0)
+    tic = time()
+    # assert(0)
 
-# main time looping
-for tout in [ud.tout]:
-    for mem in ens.members(ens):
-        Sol = mem[0]
-        flux = mem[1]
-        mpv = mem[2]
-        t = time_update(t, tout, ud, elem, node, step, th, Sol, flux, mpv, writer, debug)
-    # synchronise_variables(mpv, Sol, elem, node, ud, th)
+    # main time looping
+    for tout in [ud.tout]:
+        futures = []
+        for mem in ens.members(ens):
+            # s_ud = client.scatter(ud)
+            # time_update = time_update_wrapper(t, tout, ud, elem, node, step, th, writer=writer, debug=debug)
+            future = client.submit(time_update, *[mem[0], mem[1], mem[2], t, tout, ud, elem, node, step, th, None, debug])
+            # future = client.submit(time_update, mem)
+            # time_update(t, tout, ud, elem, node, step, th, Sol, flux, mpv, writer, debug)
+            futures.append(future)
+        client.gather(futures)
+        # synchronise_variables(mpv, Sol, elem, node, ud, th)
 
-    # print("############################################################################################")
-    # print("step %i done, t = %.12f, dt = %.12f" %(step, t, dt))
-    # print("############################################################################################")
+        # print("############################################################################################")
+        # print("step %i done, t = %.12f, dt = %.12f" %(step, t, dt))
+        # print("############################################################################################")
+        t = tout
 
-toc = time()
-print("Time taken = %.6f" %(toc-tic))
+
+    toc = time()
+    print("Time taken = %.6f" %(toc-tic))
