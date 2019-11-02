2bb6376962236476256993ac3ef68a85c72608eb
Ray Chew <ray.chew@fu-berlin.de>
2019-11-02 17:40:16 +0100

    running test for DA module on 3D Lorenz-63 model

--- RKLM_Python/data_assimilation/letkf.py
+++ RKLM_Python/data_assimilation/letkf.py
@@ -1,6 +1,7 @@
 from scipy import sparse, linalg
 from scipy.sparse.linalg import spsolve
 from scipy.ndimage import map_coordinates
+from scipy.interpolate import interpn
 from inputs.enum_bdry import BdryType
 import dask.array as darr
 import numpy as np
@@ -26,7 +27,7 @@ def da_interface(results,obs_current,attr,N,ud,loc=0):
     # print(obs_covar.shape)
     X = local_ens.analyse(obs_current.reshape(-1), obs_covar)
     local_ens.ensemble = local_ens.to_array(X)
-    local_ens.ensemble = [np.pad(mem,2,mode='constant') for mem in local_ens.ensemble]
+    local_ens.ensemble = [np.pad(mem,2,mode='wrap') for mem in local_ens.ensemble]
     # print(local_ens.X.shape)
     # return np.array([local_ens.identifier, local_ens.ensemble])
     return [local_ens.identifier, local_ens.ensemble]
@@ -40,9 +41,25 @@ def interpolation_func(ensemble,x_obs,y_obs,ud):
     # print(x_ens, y_ens, x_obs, y_obs)
     x = np.linspace(0,x_ens,x_obs)
     y = np.linspace(0,y_ens,y_obs)
+
+    # x = np.linspace(-0.5,0.5,48)
+    # y = np.linspace(-0.5,0.5,48)
+    # x = np.linspace(0,1,48)
+    # y = np.linspace(0,1,48)
+
+    # # x,y = np.meshgrid(x,y)
+    # x0 = np.linspace(-0.5,0.5,256)
+    # y0 = np.linspace(-0.5,0.5,256)
+
+    # x0 = np.linspace(0,1,256)
+    # y0 = np.linspace(0,1,256)
+    # mesh = np.array(np.meshgrid(x0,y0))
+    # pts = np.rollaxis(mesh, 0, 3).reshape((-1, 2))
+
+    # ensemble = [interpn((y,x),mem.T,pts, method='splinef2d').reshape(256,256) for mem in ensemble]
     x,y = np.meshgrid(x,y)
 
-    ensemble = [map_coordinates(mem,[y,x],mode='wrap', order=3) for mem in ensemble]
+    ensemble = [map_coordinates(mem,[x,y],mode='wrap', order=3) for mem in ensemble]
     # print(np.array(ensemble).shape)
     return np.array(ensemble)
 
@@ -55,7 +72,7 @@ class analysis(object):
         self.member_shape = self.ensemble[0].shape
 
         # ensemble inflation factor
-        self.rho = 1.5
+        self.rho = 1.0
         # if anlaysis is over local state space, which is it?
         self.identifier = identifier
 
@@ -70,10 +87,11 @@ class analysis(object):
 
     def analyse(self,obs,obs_covar):
         obs = obs.reshape(-1)
-        obs = np.random.normal(obs, obs.max()**0.5)
+        # obs = np.random.normal(obs, obs.max()**0.5)
         #obs: R in l
         #obs_covar: R in (l x l)
         # self.Y = [self.forward_operator(xi) for xi in self.X]
+
         self.Y = self.forward_operator(self.ensemble)
         self.Y = self.state_vector(self.Y)
 
@@ -83,6 +101,13 @@ class analysis(object):
         self.X_mean = self.get_mean(self.X) # R in m
         self.X -= self.X_mean # R in (m x k)
 
+
+        print("self.ensemble.shape = ", self.ensemble.shape)
+        print("self.no_of_members = ", self.no_of_members)
+        print("self.Y.shape = ", self.Y.shape)
+        print("obs.shape = ", obs.shape)
+        print("obs_covar.shape = ", obs_covar.shape)
+
         # for now, global == local, i.e. observation space == state space
 
         # here is where the R-localisation matrix will come in
@@ -90,23 +115,43 @@ class analysis(object):
         if self.localisation_matrix != None:
             obs_covar *= self.localisation_matrix
         C = spsolve(obs_covar, self.Y.T).T # R in (k x l)
+        # C = self.Y.T / obs_covar
+        # C = C.T
         # C = linalg.solve(obs_covar, self.Y.T, assume_a='pos').T # R in (k x l)
 
-        Pa = (self.no_of_members - 1.) * np.eye(self.no_of_members) / self.rho + np.dot(C,self.Y.T)
+        # print("C.shape = ", C.shape)
 
-        Lambda, P = linalg.eigh(Pa)
+        # Pa = (self.no_of_members - 1.) * np.eye(self.no_of_members) / self.rho + np.dot(C,self.Y.T)
+        # print(np.dot(C,self.Y.T).shape)
+        Rhs = (self.no_of_members - 1.) * np.eye(self.no_of_members) / self.rho + C @ self.Y.T
+        
+        Lambda, P = linalg.eigh(Rhs)
         # Lambda, P = Lambda.real, P.real
         # Pa = np.dot(P,np.dot(np.diag(1./Lambda),P.T))
-        Pa = P @ np.diag(1./Lambda) @ P.T
+        Pa = P @ (np.diag(1./Lambda) @ P.T)
 
-        Wa = (self.no_of_members - 1.)**0.5 * np.dot(P,np.dot(np.diag((1./Lambda)**0.5),P.T))
+        # print("Pa.shape = ", Pa.shape)
 
-        wa = np.dot(Pa,np.dot(C , (obs - self.Y_mean)))
+        # Wa = (self.no_of_members - 1.)**0.5 * np.dot(P,np.dot(np.diag((1./Lambda)**0.5),P.T))
+        Wa = np.sqrt((self.no_of_members - 1.)) * (P @ np.diag(np.sqrt(1./Lambda)) @ P.T)
+
+        # print(self.Y_mean.shape)
+
+        # wa = np.dot(Pa,np.dot(C , (obs - self.Y_mean) ))
+        wa = Pa @ (C * (obs - self.Y_mean))
+        # wa = np.dot(Pa,np.dot(C , (obs - self.Y_mean) - np.sqrt(obs_covar[0,0]) * np.random.randn(self.Y.shape[0])))
         # Wa += wa.reshape(1,-1)
+        # Wa = np.array([wi + wa for wi in Wa])
+
+        # Wa += wa.reshape(-1,1)
+        # print(wa.shape)
         Wa += wa
+        # print(wa)
         # print(Wa.shape)
 
-        return np.dot(self.X.T, Wa.T).T + self.X_mean
+        # return (self.X * Wa.T) + self.X_mean
+        return (self.X.T @ Wa).T + self.X_mean
+
 
     def get_mean(self,vec):
         return np.array(vec).sum(axis=0)
