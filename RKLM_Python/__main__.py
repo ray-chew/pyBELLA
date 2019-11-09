523005b848a9eb5d0f61664c4e3d5c38b199709e
Ray Chew <ray.chew@fu-berlin.de>
2019-11-09 15:20:07 +0100

    improved recognition in main.py on when to use DA

--- RKLM_Python/__main__.py
+++ RKLM_Python/__main__.py
@@ -61,7 +61,7 @@ mpv = MPV(elem, node, ud)
 
 ##########################################################
 # Data Assimilation part
-N = 10
+N = 1
 da_parameters = da_params(N)
 aprior_error_covar = da_parameters.aprior_error_covar
 
@@ -97,46 +97,47 @@ ens = ensemble(sol_ens)
 ##########################################################
 # Load observations
 # where are my observations?
-obs_path = './output_travelling_vortex/output_travelling_vortex_ensemble=1_256_256_1.0.h5'
-# obs_path = './output_travelling_vortex/output_travelling_vortex_3d_48_low_mach_gravity_comp_256_256_old.h5'
-obs_file = h5py.File(obs_path, 'r')
-#### which attributes do I want to observe?
-obs_attributes = ['rho', 'rhou', 'rhov']
-# obs_attributes = ['rho', 'rhou', 'rhov']
-# obs_attributes = ['rhou', 'rhov']
-# obs_attributes = ['rho']
-# where in the "solutions" container are they located? 0: Sol, 1: flux, 2: mpv
-loc = 0
-#### when were these observations taken?
-# times = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
-times = [0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0.90,0.95,1.0]
-# times = [0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20]
-# print(obs_file['rho'].keys())
-# assert(0)
+if N > 1:
+    obs_path = './output_travelling_vortex/output_travelling_vortex_ensemble=1_256_256_1.0.h5'
+    # obs_path = './output_travelling_vortex/output_travelling_vortex_3d_48_low_mach_gravity_comp_256_256_old.h5'
+    obs_file = h5py.File(obs_path, 'r')
+    #### which attributes do I want to observe?
+    obs_attributes = ['rho', 'rhou', 'rhov']
+    # obs_attributes = ['rho', 'rhou', 'rhov']
+    # obs_attributes = ['rhou', 'rhov']
+    # obs_attributes = ['rho']
+    # where in the "solutions" container are they located? 0: Sol, 1: flux, 2: mpv
+    loc = 0
+    #### when were these observations taken?
+    # times = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+    times = [0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0.90,0.95,1.0]
+    # times = [0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20]
+    # print(obs_file['rho'].keys())
+    # assert(0)
 
-#### axis 0 stores time series
-obs = np.empty(len(times), dtype=object)
-t_cnt = 0
-for t in times:
-    #### how were these dataset called?
-    label = '_ensemble_mem=0_%.2f_after_full_step' %t
-    #### axis 1 stores the attributes
-    obs[t_cnt] = {}
-    for attribute in obs_attributes:
-        dict_attr = {
-            attribute: obs_file[str(attribute)][str(attribute) + str(label)][:]
-        }
-        obs[t_cnt].update(dict_attr)
-    t_cnt += 1
-obs = np.array(obs)
-obs_file.close()
-ud.output_name_comp = ("_ensemble=%i_%i_%i_%.1f" %(N,elem.icx-2*elem.igx,elem.icy-2*elem.igy,ud.tout[-1]))
+    #### axis 0 stores time series
+    obs = np.empty(len(times), dtype=object)
+    t_cnt = 0
+    for t in times:
+        #### how were these dataset called?
+        label = '_ensemble_mem=0_%.2f_after_full_step' %t
+        #### axis 1 stores the attributes
+        obs[t_cnt] = {}
+        for attribute in obs_attributes:
+            dict_attr = {
+                attribute: obs_file[str(attribute)][str(attribute) + str(label)][:]
+            }
+            obs[t_cnt].update(dict_attr)
+        t_cnt += 1
+    obs = np.array(obs)
+    obs_file.close()
+    ud.output_name_comp = ("_ensemble=%i_%i_%i_%.1f" %(N,elem.icx-2*elem.igx,elem.icy-2*elem.igy,ud.tout[-1]))
 ##########################################################
 
 writer = io(ud)
 
 if __name__ == '__main__':
-    client = Client(threads_per_worker=4, n_workers=2)
+    client = Client(threads_per_worker=4, n_workers=1)
     tic = time()
     # assert(0)
 
@@ -164,42 +165,42 @@ if __name__ == '__main__':
         # print(np.array(np.where(np.isclose(times,tout))[0]))
         # print(np.isclose(times,tout))
         # print(tout, times)
-        
-        if len(np.where(np.isclose(times,tout))[0]) > 0 and N > 1:
-            print("Starting analysis...")
-            # print(np.where(times == tout)[0][0])
-            futures = []
-            # anals = [] 
-            for attr in obs_attributes:
-                print("Assimilating %s..." %attr)
-                obs_current = np.array(obs[np.where(np.isclose(times,tout))[0][0]][attr])
-                future = client.submit(da_interface, *[results,obs_current,attr,N,ud])
-                # analysis = da_interfaces(results,obs_current,attr,N,ud)
-                # anals.append(analysis)
-                futures.append(future)
+        if N > 1:
+            if len(np.where(np.isclose(times,tout))[0]) > 0:
+                print("Starting analysis...")
+                # print(np.where(times == tout)[0][0])
+                futures = []
+                # anals = [] 
+                for attr in obs_attributes:
+                    print("Assimilating %s..." %attr)
+                    obs_current = np.array(obs[np.where(np.isclose(times,tout))[0][0]][attr])
+                    future = client.submit(da_interface, *[results,obs_current,attr,N,ud])
+                    # analysis = da_interfaces(results,obs_current,attr,N,ud)
+                    # anals.append(analysis)
+                    futures.append(future)
 
-            analysis = client.gather(futures)
-            # analysis = np.array(anals)
-            analysis = np.array(analysis)
-            # print(analysis.shape)
-            # print(analysis)
-            # iterable = iter(analysis.flatten())
-            # print(iterable)
-            # analysis = dict(zip(iterable, iterable))
-            # print(analysis.keys())
+                analysis = client.gather(futures)
+                # analysis = np.array(anals)
+                analysis = np.array(analysis)
+                # print(analysis.shape)
+                # print(analysis)
+                # iterable = iter(analysis.flatten())
+                # print(iterable)
+                # analysis = dict(zip(iterable, iterable))
+                # print(analysis.keys())
 
-            print("Storing analysis...")
-            cnt = 0
-            for attr in obs_attributes:
-                # current = analysis[attr]
-            # for i in range(len(obs_attributes)):
-                current = analysis[cnt]
-                for n in range(N):
-                    # results[:,loc,...][n].attr = current[n]
-                    # print(attr)
-                    # print(results[:,loc,...][n].rho)
-                    setattr(results[:,loc,...][n],attr,current[n])
-                cnt += 1
+                print("Storing analysis...")
+                cnt = 0
+                for attr in obs_attributes:
+                    # current = analysis[attr]
+                # for i in range(len(obs_attributes)):
+                    current = analysis[cnt]
+                    for n in range(N):
+                        # results[:,loc,...][n].attr = current[n]
+                        # print(attr)
+                        # print(results[:,loc,...][n].rho)
+                        setattr(results[:,loc,...][n],attr,current[n])
+                    cnt += 1
 
         # print(results_before[:,loc,...][0].rho)
         # print(results[:,loc,...][0].rho)
@@ -207,8 +208,9 @@ if __name__ == '__main__':
              
         ens.set_members(results)
 
-        if np.allclose(ens.members(ens)[0][0].rho, results_before[:,loc,...][0].rho):
-            print("Rho quantities unchanged, i.e. no assimilation took place in rho.")
+        if N > 1:
+            if np.allclose(ens.members(ens)[0][0].rho, results_before[:,loc,...][0].rho):
+                print("Rho quantities unchanged, i.e. no assimilation took place in rho.")
 
         print("Starting output...")
         for n in range(N):
