0988b1eb07d1c9e1ef0aa54dd7db2ed83a821e50
Ray Chew <ray.chew@fu-berlin.de>
2019-11-14 18:29:37 +0100

    updated sphinx conf.py

--- RKLM_Python/docs/conf.py
+++ RKLM_Python/docs/conf.py
@@ -58,6 +58,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 import wild_sphinx_theme
 html_theme = 'wild'
 html_theme_path = [wild_sphinx_theme.get_theme_dir()]
+#import sphinx_rtd_theme
+#extensions = ["sphinx_rtd_theme"]
+#html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
