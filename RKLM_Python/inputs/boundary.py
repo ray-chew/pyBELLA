fc78cc36a4d4ce0798ae5b9fc071fed45990a4e5
Ray Chew <ray.chew@fu-berlin.de>
2019-11-14 18:48:41 +0100

    added comments and sphinx docstrings for boundary.py

--- RKLM_Python/inputs/boundary.py
+++ RKLM_Python/inputs/boundary.py
@@ -1,4 +1,6 @@
-from management.enumerator import HillShapes
+"""
+For more details on this module, refer to the write-up :ref:`boundary_handling`.
+"""
 from inputs.enum_bdry import BdryType
 
 import numpy as np
@@ -6,8 +8,29 @@ import numpy as np
 from management.debug import find_nearest
 
 def set_explicit_boundary_data(Sol, elem, ud, th, mpv, step=None):
+    """
+    In-place update of the ghost cells in :class:`management.variable.Vars` given the boundary conditions specified by :class:`inputs.user_data.UserDataInit`.
+
+    Parameters
+    ----------
+    Sol : :class:`management.variable.Vars`
+        Solution data container.
+    elem : :class:`discretization.kgrid.ElemSpaceDiscr`
+        Cell grid.
+    ud : :class:`inputs.user_data.UserDataInit`
+        Data container for the initial conditions
+    th : :class:`physics.gas_dynamics.thermodynamic.ThemodynamicInit`
+        Thermodynamic variables of the system.
+    mpv :
+        Variables relating to the elliptic solver
+    step : int, optional
+        Current step
+
+    """
     igs = elem.igs
     ndim = elem.ndim
+
+    # if step parameter is not None, then we are in the advection directional Strang-splitting, where the array has already flipped, and we should only update the relevant boundaries, i.e. those in the direction of the current Strang-split-step.
     if step == None:
         dims = np.arange(ndim)
     else:
@@ -18,36 +41,31 @@ def set_explicit_boundary_data(Sol, elem, ud, th, mpv, step=None):
             current_step = step
         else:
             current_step = dim
-
-        # print("dim = ", dim)
-        # print("dims = ", dims)
-        # print("step = ", step)
         ghost_padding, idx = get_ghost_padding(ndim,dim,igs)
-        # print("current_step = ", current_step)
 
         if ud.gravity_strength[current_step] == 0.0:
+            # Do this for the axes that do not have gravity.
+            # Periodic BC.
             if ud.bdry_type[current_step] == BdryType.PERIODIC:
-                # print("PERIODIC, dim = ", dim)
-                # print(ghost_padding)
                 set_boundary(Sol,ghost_padding,'wrap',idx,step=None)
+            # Wall BC.
             else:
-                # print("WALL, dim = ", dim)
                 set_boundary(Sol,ghost_padding,'symmetric',idx,step=None)
 
         else:
-            # recursive updating of array - for loop cannot be avoided....?
-            # assumption: gravity always acts in the y-axis
-            # print("Gravity = True, current_step = %i, dim = %i" %(current_step, dim))
+            # get current axis that has gravity.
             gravity_axis = dim
-            #     gravity_axis = 1
             
             direction = -1.
             offset = 0
 
+            # get gravity strength specified in the user data file.
             g = ud.gravity_strength[gravity_axis]
 
+            # for the number of ghost cells in the gravity axis...
             for side in ghost_padding[gravity_axis]:
                 direction *= -1
+                # loop through each of these ghost cells.
                 for current_idx in np.arange(side)[::-1]:
                     nlast, nsource, nimage = get_gravity_padding(ndim,current_idx,direction,offset,elem)
 
@@ -82,6 +100,26 @@ def set_explicit_boundary_data(Sol, elem, ud, th, mpv, step=None):
 
 
 def set_boundary(Sol,pads,btype,idx,step=None):
+    """
+    Called by the function :func:`inputs.boundary.set_explicit_boundary_data`. Pads in-place the ghost cells for a given boundary type.
+
+    Parameters
+    ----------
+    Sol : :class:`management.variable.Vars`
+        Solution data container.
+    pads : tuple
+        A tuple containing the number of ghost cells to pad at each end.
+    btype : string
+        The type of boundary condition to pad. Currently supports:
+            * `wrap` for periodic boundary conditions
+            * `symmetric` for wall boundary conditions
+            * `negative_symmetric` for wall boundary conditions, but with the signs flipped.
+    idx : tuple
+        A tuple containing the slice indices for the inner array, e.g. `(slice(2,-2),slice(2,-2))` for a 2D-array with 2 ghost cells for all edges.
+    step : int, optional
+        If we are in the advection routine with the flipped arrays according to the directional Strang-splitting, we want to pad the correct direction. `step=0`, pads the x-direction while `step=1` pads the y-direction.
+
+    """
     Sol.rho[...] = np.pad(Sol.rho[idx],pads,btype)
     Sol.rhou[...] = np.pad(Sol.rhou[idx],pads,btype)
     if step == None or step == 0:
@@ -91,7 +129,6 @@ def set_boundary(Sol,pads,btype,idx,step=None):
         else:
             Sol.rhov[...] = np.pad(Sol.rhov[idx],pads,btype)
 
-    # print(pads)
     if step == 1:
         Sol.rhov[...] = np.pad(Sol.rhov[idx],pads,btype)
         if btype == 'symmetric':
@@ -106,6 +143,23 @@ def set_boundary(Sol,pads,btype,idx,step=None):
 
 
 def negative_symmetric(vector,pad_width,iaxis,kwargs=None):
+    """
+    For more details, refer to: https://docs.scipy.org/doc/numpy/reference/generated/numpy.pad.html.
+
+    Taken from the link:
+
+    Parameters
+    ----------
+    vector : ndarray
+        A rank 1 array already padded with zeros. Padded values are vector `[:iaxis_pad_width[0]] and vector[-iaxis_pad_width[1]:]`.
+    iaxis_pad_width : tuple
+        A 2-tuple of ints, `iaxis_pad_width[0]` represents the number of values padded at the beginning of vector where `iaxis_pad_width[1]` represents the number of values padded at the end of vector.
+    iaxis : int
+        The axis currently being calculated.
+    kwargs : dict
+        Any keyword arguments the function requires.
+
+    """
     if pad_width[1] > 0:
         sign = -1.
         vector[:pad_width[0]] = sign * vector[pad_width[0]:2*pad_width[0]][::-1]
@@ -116,6 +170,23 @@ def negative_symmetric(vector,pad_width,iaxis,kwargs=None):
 
 
 def get_gravity_padding(ndim,cur_idx,direction,offset,elem,y_axs=None):
+    """
+    Parameters
+    ----------
+    ndim : int
+        Number of dimensions.
+    cur_idx : int
+        The current index of the ghost cell in the gravity direction to be updated.
+    direction : int
+        Top of the domain, `direction=+1`, bottom of the domain, `direction=-1`.
+    offset : int
+        ?
+    elem : :class:`discretization.kgrid.ElemSpaceDiscr`
+        Cell grid.
+    y_axs : int, optional
+        `Default == None`. Specifies the direction of the gravity axis. If `None`, then direction is the the y-axis.
+
+    """
     cur_i = np.copy(cur_idx)
     cur_idx += offset * ((elem.icy - 1) - 2*cur_idx)
     gravity_padding = [(slice(None))] * ndim
@@ -157,6 +228,26 @@ def set_ghostnodes_p2(p,node,ud):
 
 
 def get_ghost_padding(ndim,dim,igs):
+    """
+    For a given direction, return the number of ghost cells to pad the current direction, and the index slice of the inner array without the ghost cells.
+
+    Parameters
+    ----------
+    ndim : int
+        Number of dimensions for the problem.
+    dim : int
+        Current dimension to update
+    igs : list
+        A list of number of ghost cells in all dimensions, e.g. `[2,2,2]` for 2 ghost cells in the x, y, and z directions.
+
+    Returns
+    -------
+    tuple
+        Number of ghost cells in the current dimension at both edges.
+    tuple
+        Index slice of the inner domain of the array.
+
+    """
     ghost_padding = [(0,0)] * ndim
     ghost_padding[dim] = (igs[dim],igs[dim])
 
@@ -172,61 +263,78 @@ def get_ghost_padding(ndim,dim,igs):
 
 
 def periodic_plus_one(vector, pad_width, iaxis, kwargs=None):
+    """
+    For more details, refer to: https://docs.scipy.org/doc/numpy/reference/generated/numpy.pad.html.
+
+    Taken from the link:
+
+    Parameters
+    ----------
+    vector : ndarray
+        A rank 1 array already padded with zeros. Padded values are vector `[:iaxis_pad_width[0]] and vector[-iaxis_pad_width[1]:]`.
+    iaxis_pad_width : tuple
+        A 2-tuple of ints, `iaxis_pad_width[0]` represents the number of values padded at the beginning of vector where `iaxis_pad_width[1]` represents the number of values padded at the end of vector.
+    iaxis : int
+        The axis currently being calculated.
+    kwargs : dict
+        Any keyword arguments the function requires.
+
+    """
     if all(pad_width) > 0:
         vector[:pad_width[0]+1], vector[-pad_width[1]-1:] = vector[-pad_width[1]-pad_width[1]-1:-pad_width[1]] , vector[pad_width[0]:pad_width[0]+pad_width[0]+1].copy()
     return vector
 
 
-def check_flux_bcs(Lefts, Rights, elem, split_step, ud):
-    igx = elem.igx
-    igy = elem.igy
+# def check_flux_bcs(Lefts, Rights, elem, split_step, ud):
+#     igx = elem.igx
+#     igy = elem.igy
 
-    if split_step == 1:
-        if ud.bdry_type[split_step] == BdryType.WALL:
+#     if split_step == 1:
+#         if ud.bdry_type[split_step] == BdryType.WALL:
 
-            left_inner = (slice(None),slice(igy,igy+1))
-            left_ghost = (slice(None),slice(igy-1,igy))
+#             left_inner = (slice(None),slice(igy,igy+1))
+#             left_ghost = (slice(None),slice(igy-1,igy))
 
-            right_inner = (slice(None),slice(-igy-1,-igy))
-            right_ghost = (slice(None),slice(-igy,-igy+1))
+#             right_inner = (slice(None),slice(-igy-1,-igy))
+#             right_ghost = (slice(None),slice(-igy,-igy+1))
 
-            rhou_wall = 0.
-            # Lefts.rhou[left_ghost] = Rights.rhou[left_inner] = rhou_wall
-            Lefts.rhoY[left_ghost] = Rights.rhoY[left_inner] = Rights.rho[left_inner] * ud.stratification(0.0)
+#             rhou_wall = 0.
+#             # Lefts.rhou[left_ghost] = Rights.rhou[left_inner] = rhou_wall
+#             Lefts.rhoY[left_ghost] = Rights.rhoY[left_inner] = Rights.rho[left_inner] * ud.stratification(0.0)
 
-            Lefts.rho[left_ghost] = Rights.rho[left_inner]
-            Lefts.rhov[left_ghost] = Rights.rhov[left_inner]
-            Lefts.rhow[left_ghost] = Rights.rhow[left_inner]
-            Lefts.rhoe[left_ghost] = Rights.rhoe[left_inner]
-            Lefts.rhoX[left_ghost] = Rights.rhoX[left_inner]
-            # print(Lefts.rhoY[left_ghost])
-            # print(Rights.rho[0,:])
+#             Lefts.rho[left_ghost] = Rights.rho[left_inner]
+#             Lefts.rhov[left_ghost] = Rights.rhov[left_inner]
+#             Lefts.rhow[left_ghost] = Rights.rhow[left_inner]
+#             Lefts.rhoe[left_ghost] = Rights.rhoe[left_inner]
+#             Lefts.rhoX[left_ghost] = Rights.rhoX[left_inner]
+#             # print(Lefts.rhoY[left_ghost])
+#             # print(Rights.rho[0,:])
 
-            Rights.rho[right_ghost] = Lefts.rho[right_inner]
-            Rights.rhou[right_ghost] = -1. * Lefts.rhou[right_inner]
-            Rights.rhov[right_ghost] = Lefts.rhov[right_inner]
-            Rights.rhow[right_ghost] = Lefts.rhow[right_inner]
-            Rights.rhoe[right_ghost] = Lefts.rhoe[right_inner]
-            Rights.rhoY[right_ghost] = Lefts.rhoY[right_inner]
+#             Rights.rho[right_ghost] = Lefts.rho[right_inner]
+#             Rights.rhou[right_ghost] = -1. * Lefts.rhou[right_inner]
+#             Rights.rhov[right_ghost] = Lefts.rhov[right_inner]
+#             Rights.rhow[right_ghost] = Lefts.rhow[right_inner]
+#             Rights.rhoe[right_ghost] = Lefts.rhoe[right_inner]
+#             Rights.rhoY[right_ghost] = Lefts.rhoY[right_inner]
 
-            # print(Lefts.rhoY[:,-igx-2])
+#             # print(Lefts.rhoY[:,-igx-2])
             
-    else:
-        if ud.bdry_type[split_step] == BdryType.WALL:
+#     else:
+#         if ud.bdry_type[split_step] == BdryType.WALL:
 
-            assert(0) # INCOMPLETE!!!
-            Lefts.rho[left_inner] = Rights.rho[:,igx-2]
-            Lefts.rhou[left_inner] = -1. * Rights.rhou[:,igx-2]
-            Lefts.rhov[left_inner] = Rights.rhov[:,igx-2]
-            Lefts.rhow[left_inner] = Rights.rhow[:,igx-2]
-            Lefts.rhoe[left_inner] = Rights.rhoe[:,igx-2]
-            Lefts.rhoY[left_inner] = Rights.rhoY[:,igx-2]
+#             assert(0) # INCOMPLETE!!!
+#             Lefts.rho[left_inner] = Rights.rho[:,igx-2]
+#             Lefts.rhou[left_inner] = -1. * Rights.rhou[:,igx-2]
+#             Lefts.rhov[left_inner] = Rights.rhov[:,igx-2]
+#             Lefts.rhow[left_inner] = Rights.rhow[:,igx-2]
+#             Lefts.rhoe[left_inner] = Rights.rhoe[:,igx-2]
+#             Lefts.rhoY[left_inner] = Rights.rhoY[:,igx-2]
 
-            # print("#################### TRUE ########################")
-            Rights.rho[right_ghost] = Lefts.rho[:,-igx-2]
-            Rights.rhou[right_ghost] = -1. * Lefts.rhou[:,-igx-2]
-            Rights.rhov[right_ghost] = Lefts.rhov[:,-igx-2]
-            Rights.rhow[right_ghost] = Lefts.rhow[:,-igx-2]
-            Rights.rhoe[right_ghost] = Lefts.rhoe[:,-igx-2]
-            Rights.rhoY[right_ghost] = Lefts.rhoY[:,-igx-2]
-            # print(Rights.rhoY[right_ghost])
+#             # print("#################### TRUE ########################")
+#             Rights.rho[right_ghost] = Lefts.rho[:,-igx-2]
+#             Rights.rhou[right_ghost] = -1. * Lefts.rhou[:,-igx-2]
+#             Rights.rhov[right_ghost] = Lefts.rhov[:,-igx-2]
+#             Rights.rhow[right_ghost] = Lefts.rhow[:,-igx-2]
+#             Rights.rhoe[right_ghost] = Lefts.rhoe[:,-igx-2]
+#             Rights.rhoY[right_ghost] = Lefts.rhoY[:,-igx-2]
+#             # print(Rights.rhoY[right_ghost])
