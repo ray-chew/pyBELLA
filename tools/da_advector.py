253d9eb4f63ef8945de25ee3a2eb87d85376feb1
Ray Chew <ray.chew@fu-berlin.de>
2020-02-08 15:36:16 +0100

    da_advector: added comments and batch obs DA method - runs

--- tools/da_advector.py
+++ tools/da_advector.py
@@ -1,7 +1,6 @@
 import sys
 import os
 sys.path.insert(0, os.getcwd() + '/RKLM_Python/')
-print(sys.path)
 
 from inputs import travelling_vortex_3D as tv
 from inputs.user_data import UserDataInit
@@ -14,13 +13,20 @@ from physics.gas_dynamics import thermodynamic as thmdyn
 from physics.gas_dynamics import numerical_flux as nf
 from physics.low_mach import mpv as m
 
-from data_assimilation import utils as utils
+from data_assimilation import utils
+from data_assimilation import params
+from data_assimilation import letkf
 
 import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
 from time import time
 
+
+######################################
+#
+# initialise arrays
+#
 initial_data = vars(tv.UserData())
 ud = UserDataInit(**initial_data)
 elem, node = data.data_init(ud)
@@ -36,23 +42,53 @@ if elem.ndim > 2:
     flux[2] = var.States(elem.sfz, ud)
 
 th = thmdyn.ThemodynamicInit(ud)
-
 mpv = m.MPV(elem, node, ud)
+#
+######################################
 
-N = 1
+
+######################################
+#
+# initialise simulation parameters
+#
+N = 2                   # no. of ensemble members.
 step = 0
 t = 0.0
-dt = 0.001
+dt = 0.001              # temporal step-size
 
-label_type = 'STEP'
+label_type = 'STEP'     # output time-series in terms time or step?
+#
+######################################
 
+######################################
+#
+# initialise data assimilation
+# parameters
+#
+dap = params.da_params(N,da_type='batch_obs')
+obs = dap.load_obs(dap.obs_path)
+#
+######################################
+
+
+######################################
+#
+# initialise IO writer
+#
 ud.output_suffix = '_ensemble=%i%s' %(N, ud.output_suffix)
 
 writer = io.io(ud)
 writer.write_attrs()
 if N > 1:
-    writer.write_da_attrs(da_parameters)
+    writer.write_da_attrs(dap)
+#
+######################################
 
+
+######################################
+#
+# initialise ensemble
+#
 print("Generating initial ensemble...")
 sol_ens = np.zeros((N), dtype=object)
 np.random.seed(555)
@@ -67,9 +103,14 @@ if seeds[0] != None:
 else:
     sol_ens = [[sol_init(Sol, mpv, elem, node, th, ud),flux,mpv,step]]
 ens = utils.ensemble(sol_ens)
+#
+######################################
 
-# print(ens)
 
+######################################
+#
+# write initial conditions
+#
 for n in range(N):
     Sol = ens.members(ens)[n][0]
     mpv = ens.members(ens)[n][2]
@@ -81,17 +122,30 @@ for n in range(N):
     else:
         label = ('%s%.2f' %(ens_str,0.0))
     writer.write_all(Sol,mpv,elem,node,th,str(label)+'_ic')
+#
+######################################
 
+
+######################################
+#
+# start simulation
+#
 tic = time()
 tout_cnt = 0
 for tout in ud.tout:
+
+    ######################################
+    #
+    # start advection routine for each 
+    # ensemble member
+    #
     print('##############################################')
     print('Next tout = %.3f' %tout)
     print("Starting forecast...")
+    results = []
     for mem in ens.members(ens):
-        print(mem)
         Sol, flux, mpv = mem[0], mem[1], mem[2]
-        
+
         window_step = 0
         u,v = np.ones_like(Sol.rho), np.ones_like(Sol.rho)
 
@@ -101,6 +155,53 @@ for tout in ud.tout:
         
             t += dt
             window_step += 1
+        results.append([Sol,flux,mpv])
+    results = np.array(results)
+    #
+    # end advection routine
+    #
+    ######################################
+
+
+    ######################################
+    #
+    # start data assimilation step 
+    #
+    if N > 1 and tout in dap.da_times:
+
+        ######################################
+        #
+        # localisation: batch observations
+        #
+        if dap.da_type == 'batch_obs':
+            print("Starting analysis... for batch observations")
+            analysis = []
+            for attr in dap.obs_attributes:
+                print("Assimilating %s..." %attr)
+
+                obs_current = np.array(obs[np.argwhere(dap.da_times == tout)[0][0]][attr])
+
+                analysis.append(letkf.da_interface(results,obs_current,dap.inflation_factor,attr,N,ud))
+
+            analysis = np.array(analysis)
+
+            print("Writing analysis...")
+            cnt = 0
+            for attr in dap.obs_attributes:
+                current = analysis[cnt]
+                for n in range(N):
+                    setattr(results[:,dap.loc,...][n],attr,current[n])
+                cnt += 1
+        #
+        # end localisation: batch observations
+        #
+        ###################################### 
+        
+        
+    #
+    # end data assimilation step
+    #
+    ######################################
 
     print("Starting output...")
     for n in range(N):
@@ -116,3 +217,12 @@ for tout in ud.tout:
 
     step += 1
     t = tout
+
+toc = time()
+print("Time taken = %.6f" %(toc-tic))
+
+writer.close_everything()
+#
+# end simulation
+#
+######################################
