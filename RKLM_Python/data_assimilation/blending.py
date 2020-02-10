f21e59605fe3e77cd21f035b6f55f2779f54158e
Ray Chew <ray.chew@fu-berlin.de>
2020-02-10 18:28:58 +0100

    blending: latest stand of the blending class - something is still not quite right...

--- RKLM_Python/data_assimilation/blending.py
+++ RKLM_Python/data_assimilation/blending.py
@@ -2,10 +2,7 @@ import numpy as np
 from scipy import signal
 from inputs import boundary
 
-def do():
-    None
-
-class blend(object):
+class Blend(object):
     def __init__(self,ud):
         self.bb = False
         self.cb = ud.continuous_blending
@@ -30,43 +27,56 @@ class blend(object):
         None
         
     def convert_p2n(self, p2n, p2n0):
-        self.ps = p2n0
-        self.pn = p2n
+        self.ps = p2n0 - p2n0.mean()
+        self.pn = p2n - p2n.mean()
 
-        ndim =  pn.ndim
+        ndim =  self.pn.ndim
 
-        p2n -= p2n.mean()
-        p2n0 -= p2n0.mean()
-        dp2n = 0.5 * (p2n + p2n0)
+        if self.cb and not self.bb:
+            dp2n = p2n0
+            assert(0)
+        else:
+            # dp2n = 0.5*(self.ps + self.pn)
+            dp2n = p2n0
 
         self.kernel = np.ones([2]*ndim)
         dp2c = signal.fftconvolve(dp2n,self.kernel, mode='valid') / self.kernel.sum()
 
-        self.dp2n = dp2n
-        self.dp2c = dp2c
+        self.pnc = signal.fftconvolve(self.pn,self.kernel, mode='valid') / self.kernel.sum()
+
+        self.psc = signal.fftconvolve(self.ps,self.kernel, mode='valid') / self.kernel.sum()
+
+        self.dp2n = dp2n - dp2n.mean()
+        self.dp2c = dp2c - dp2c.mean()
 
         return dp2c
 
-    def update_Sol(self, Sol, th):
-        # rho = np.copy(Sol.rho)
-        # rhoY = np.copy(Sol.rhoY)
+    def update_Sol(self, Sol, th, ud):
+        rho = np.copy(Sol.rho)
+        rhoY = np.copy(Sol.rhoY)
+
+        Y = rhoY / rho
 
         Sol.rhoY = (Sol.rhoY**th.gm1 + self.fac * self.dp2c)**(th.gm1inv)
 
-        # Sol.rho[...] = Sol.rhoY / Y
+        Sol.rho[...] = Sol.rhoY / Y
 
-        # rho_fac = Sol.rho / rho_psinc
-        # Sol.rhou[...] *= rho_fac
-        # Sol.rhov[...] *= rho_fac
-        # Sol.rhow[...] *= rho_fac
-        # Sol.rhoX[...] *= rho_fac
+        rho_fac = Sol.rho / rho
+        Sol.rhou[...] *= rho_fac
+        Sol.rhov[...] *= rho_fac
+        Sol.rhow[...] *= rho_fac
+        Sol.rhoX[...] *= rho_fac
 
-    def update_p2n(self,Sol, node, th, ud):
-        # psm = self.ps - self.ps.mean()
-        rhoYncomp = np.zeros_like(self.dp2n)
-        rhoYncomp[1:-1,1:-1] = signal.fftconvolve(Sol.rhoY,self.kernel,mode='valid') / self.kernel.sum()
-        boundary.set_ghostnodes_p2(rhoYncomp,node,ud)
-        return (rhoYncomp**th.gm1 - 1.0 + self.dp2n)
+    def update_p2n(self,Sol, mpv, node, th, ud):
+        # rhoYncomp = np.zeros_like(self.dp2n)
+        # rhoYncomp[1:-1,1:-1] = signal.fftconvolve(Sol.rhoY,self.kernel,mode='valid') / self.kernel.sum()
+        # boundary.set_ghostnodes_p2(rhoYncomp,node,ud)
+        # mpv.p2_nodes[...] = (rhoYncomp**th.gm1) - 1.0 + self.dp2n
+        # mpv.p2_nodes -= mpv.p2_nodes.mean()
+
+        # pn = self.pn - self.pn.mean()
+        # ps = self.ps - self.ps.mean()
+        mpv.p2_nodes[...] = self.dp2n
 
     def walk_half_step(self):
         None
