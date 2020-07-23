80dcd93a7f1d9da40e89163da3a1f2bedeb40283
Ray Chew <ray.chew@fu-berlin.de>
2020-07-23 14:58:14 +0200

    2D VS outputs z-cofficients for elliptic solve
    the output is just an array of zeros, but this helps in writing diagnostics / comparisons with 3D solutions

--- RKLM_Python/physics/low_mach/second_projection.py
+++ RKLM_Python/physics/low_mach/second_projection.py
@@ -185,6 +185,7 @@ def euler_backward_non_advective_impl_part(Sol, mpv, elem, node, ud, th, t, dt, 
         writer.populate(str(label),'wplusx',mpv.wplus[0])
         writer.populate(str(label),'wplusy',mpv.wplus[1])
         if elem.ndim == 3: writer.populate(str(label),'wplusz',mpv.wplus[2])
+        else: writer.populate(str(label),'wplusz',np.zeros_like(mpv.wplus[0]))
 
     rhs[...], _ = divergence_nodes(rhs,elem,node,Sol,ud)
     rhs /= dt
