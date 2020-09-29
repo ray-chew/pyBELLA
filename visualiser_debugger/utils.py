3d1d51c9c50580ecc317bd07dbc6d9cec3152c88
Ray Chew <ray.chew@fu-berlin.de>
2020-09-29 22:57:09 +0200

    extended utils.py to load pickled instances

--- visualiser_debugger/utils.py
+++ visualiser_debugger/utils.py
@@ -66,11 +66,11 @@ class test_case(object):
         }
         return dd
 
-    def get_filename(self,N,suffix):
+    def get_filename(self,N,suffix,format='h5'):
         if self.ndim == 2:
-            fn = "%s_ensemble=%i_%i_%i_%.1f_%s.h5" %(self.base_fn,N,self.grid_x,self.grid_y,self.end_time,suffix)
+            fn = "%s_ensemble=%i_%i_%i_%.1f_%s.%s" %(self.base_fn,N,self.grid_x,self.grid_y,self.end_time,suffix,format)
         if self.ndim == 3 or self.grid_z is not None:
-            fn = "%s_ensemble=%i_%i_%i_%i_%.1f_%s.h5" %(self.base_fn,N,self.grid_x,self.grid_y,self.grid_z,self.end_time,suffix)
+            fn = "%s_ensemble=%i_%i_%i_%i_%.1f_%s.%s" %(self.base_fn,N,self.grid_x,self.grid_y,self.grid_z,self.end_time,suffix, format)
         return fn
 
 
