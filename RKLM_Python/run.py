d9d1a7497e5421105f339538671482fbbf7bfc9d
Ray Chew <ray.chew@fu-berlin.de>
2020-11-05 17:45:12 +0100

    queue_run.py for remote queues seem to run for ud updates

--- RKLM_Python/run.py
+++ RKLM_Python/run.py
@@ -5,27 +5,51 @@ import numpy as np
 import os
 os.chdir('../')
 
-N = 10
 
-restart = False
-#########################################################
-# simulation restart parameters
-#########################################################
-path = '/home/ray/git-projects/RKLM_Reference/output_swe/'
-fn = 'output_swe_ensemble=1_64_1_64_86400.0_dvortex_3D.h5'
-path += fn
-name = '_ensemble_mem=0_86400.000_after_full_step'
-# time = np.arange(432000.0,864000.0+1200,1200)
-time_start = 86400.0
-time_end = 86400.0*2.0+1200
-time_int = 1200.0
+class run_params(object):
+    N = 1
+    tc = 'tv'
 
-time_start, time_end, time_int = str(time_start), str(time_end), str(time_int)
+    def __init__(self):
+        self.N = self.N
+        self.tc = self.tc
 
-#########################################################
+        self.ud = "None"
+        self.params = "None"
+        self.restart = False
 
-if restart == False:
-    subprocess.call([sys.executable, './RKLM_Python/__main__.py', '-ic', 'tv', '-N', '%i' %N])
+    def single_run(self):
+        if self.restart == False:
+            subprocess.call([sys.executable, './RKLM_Python/__main__.py', '-ic', self.tc, '-N', '%i' %self.N])
 
-elif restart == True:
-    subprocess.call([sys.executable, './RKLM_Python/__main__.py', '-ic', 'swe_test', 'restart', '-p', path, '-n', name, '-t', time_start, time_end, time_int])
+    def queue_run(self):
+        if self.ud is None and self.params is None:
+            assert 0, "ud or params must be defined"
+        subprocess.call([sys.executable, './RKLM_Python/__main__.py', '-ic', self.tc, '-N', '%i' %self.N, 'queue', '-w', self.ud, self.params])
+
+if __name__ == '__main__':
+    rp = run_params()
+    rp.single_run()
+
+    restart = False
+    #########################################################
+    # simulation restart parameters
+    #########################################################
+    path = '/home/ray/git-projects/RKLM_Reference/output_swe/'
+    fn = 'output_swe_ensemble=1_64_1_64_86400.0_dvortex_3D.h5'
+    path += fn
+    name = '_ensemble_mem=0_86400.000_after_full_step'
+    # time = np.arange(432000.0,864000.0+1200,1200)
+    time_start = 86400.0
+    time_end = 86400.0*2.0+1200
+    time_int = 1200.0
+
+    time_start, time_end, time_int = str(time_start), str(time_end), str(time_int)
+
+    #########################################################
+
+    # if restart == False:
+    #     subprocess.call([sys.executable, './RKLM_Python/__main__.py', '-ic', 'tv', '-N', '%i' %N])
+
+    # elif restart == True:
+    #     subprocess.call([sys.executable, './RKLM_Python/__main__.py', '-ic', 'swe_test', 'restart', '-p', path, '-n', name, '-t', time_start, time_end, time_int])
