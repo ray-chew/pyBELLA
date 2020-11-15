56350acc7d8568d61c1bfd82018e88a0aeeb9066
Ray Chew <ray.chew@fu-berlin.de>
2020-11-15 17:09:55 +0100

    push IC and switch updates for merge

--- RKLM_Python/queue_run.py
+++ RKLM_Python/queue_run.py
@@ -4,11 +4,11 @@ import json
 rp = rp()
 
 rp.N = 1
-rp.tc = 'tv'
+rp.tc = 'swe_dvortex'
 ud = {
-    # 'inx' : 128+1,
+    # 'inx' : 64+1,
     # 'iny' : 128+1,
-    'aux' : 'debug'
+    'aux' : 'debug_incompressible'
 }
 
 dap = {
@@ -19,19 +19,19 @@ rp.ud = json.dumps(ud)
 rp.dap = json.dumps(dap)
 rp.queue_run()
 
-rp.N = 1
-rp.tc = 'tv'
-ud = {
-    # 'inx' : 128+1,
-    # 'iny' : 128+1,
-    'aux' : 'debug_1',
-    'initial_blending' : False
-}
+# rp.N = 1
+# rp.tc = 'tv'
+# ud = {
+#     # 'inx' : 128+1,
+#     # 'iny' : 128+1,
+#     'aux' : 'debug_1',
+#     'initial_blending' : False
+# }
 
-dap = {
-    'noise_percentage' : 0.2,
+# dap = {
+#     'noise_percentage' : 0.2,
 
-}
-rp.ud = json.dumps(ud)
-rp.dap = json.dumps(dap)
-rp.queue_run()
+# }
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
