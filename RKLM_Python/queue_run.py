610d2e745a3830f7d2a96dde99719e8cac3a871f
Ray Chew <ray.chew@fu-berlin.de>
2020-11-15 17:46:02 +0100

    remote queue for higher resolution simulations

--- RKLM_Python/queue_run.py
+++ RKLM_Python/queue_run.py
@@ -4,11 +4,27 @@ import json
 rp = rp()
 
 rp.N = 1
-rp.tc = 'tv'
+rp.tc = 'swe_dvortex'
 ud = {
-    # 'inx' : 64+1,
-    # 'iny' : 128+1,
-    'aux' : 'debug_rkadv_psinc'
+    'inx' : 64+1,
+    'inz' : 64+1,
+    'aux' : 'debug_dsi_da_incompressible'
+}
+
+dap = {
+    'noise_percentage' : 0.1,
+
+}
+
+rp.ud = json.dumps(ud)
+rp.dap = json.dumps(dap)
+rp.queue_run()
+rp.N = 1
+rp.tc = 'swe_dvortex'
+ud = {
+    'inx' : 128+1,
+    'inz' : 128+1,
+    'aux' : 'debug_dsi_da_incompressible'
 }
 
 dap = {
@@ -19,19 +35,18 @@ rp.ud = json.dumps(ud)
 rp.dap = json.dumps(dap)
 rp.queue_run()
 
-# rp.N = 1
-# rp.tc = 'tv'
-# ud = {
-#     # 'inx' : 128+1,
-#     # 'iny' : 128+1,
-#     'aux' : 'debug_1',
-#     'initial_blending' : False
-# }
+rp.N = 1
+rp.tc = 'swe_dvortex'
+ud = {
+    'inx' : 256+1,
+    'inz' : 256+1,
+    'aux' : 'debug_dsi_da_incompressible'
+}
 
-# dap = {
-#     'noise_percentage' : 0.2,
+dap = {
+    'noise_percentage' : 0.1,
 
-# }
-# rp.ud = json.dumps(ud)
-# rp.dap = json.dumps(dap)
-# rp.queue_run()
+}
+rp.ud = json.dumps(ud)
+rp.dap = json.dumps(dap)
+rp.queue_run()
