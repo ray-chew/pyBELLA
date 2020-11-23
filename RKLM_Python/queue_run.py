6ba93f98905abff011dee80dfc2d7346b4e1d5e8
Ray Chew <ray.chew@fu-berlin.de>
2020-11-23 20:33:24 +0100

    latest IC files

--- RKLM_Python/queue_run.py
+++ RKLM_Python/queue_run.py
@@ -4,34 +4,40 @@ import json
 rp = rp()
 
 rp.N = 1
-rp.tc = 'tv'
+rp.tc = 'rb'
 ud = {
     # 'inx' : 64+1,
     # 'iny' : 128+1,
-    'aux' : 'debug_rkadv_psinc'
+    # 'aux' : 'comp_bal_noib'
+    # 'aux' : 'comp_imbal_full'
+    'aux' : 'debug_rkadv_comp_CFL_idt=test1'
+    # 'None' : None
 }
 
 dap = {
-    'noise_percentage' : 0.1,
-
+    # 'noise_percentage' : 0.1,
+    'None' : None
 }
+
 rp.ud = json.dumps(ud)
 rp.dap = json.dumps(dap)
 rp.queue_run()
 
-# rp.N = 1
-# rp.tc = 'tv'
 # ud = {
-#     # 'inx' : 128+1,
-#     # 'iny' : 128+1,
-#     'aux' : 'debug_1',
-#     'initial_blending' : False
+#     'is_compressible' : 1,
+#     'compressibility' : 1.0,
+#     'aux' : 'debug_rkadv_comp_CFL_idt=1'
 # }
 
-# dap = {
-#     'noise_percentage' : 0.2,
-
-# }
 # rp.ud = json.dumps(ud)
-# rp.dap = json.dumps(dap)
+# rp.queue_run()
+
+# ud = {
+#     'is_compressible' : 1,
+#     'compressibility' : 1.0,
+#     'initial_blending' : True,
+#     'aux' : 'debug_rkadv_comp_CFL_idt=1'
+# }
+
+# rp.ud = json.dumps(ud)
 # rp.queue_run()
