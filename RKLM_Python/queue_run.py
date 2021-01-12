f2bca629856a897a1a56fcb0d424bd9be282ff3b
Ray Chew <ray.chew@fu-berlin.de>
2021-01-12 21:41:23 +0100

    changes to driver files

--- RKLM_Python/queue_run.py
+++ RKLM_Python/queue_run.py
@@ -3,37 +3,70 @@ import json
 
 rp = rp()
 
-# rp.N = 1
-# rp.tc = 'tv'
-# ud = {
-#     'aux' : 'comp_bal',
-#     # 'continuous_blending' : False
-# }
+##########################################
+#
+# TV queue, obs RMSEs
+#
+##########################################
 
-# dap = {
-#     'None' : None
-# }
+rp.N = 10
+rp.tc = 'tv_neg'
+ud = {
+    'aux' : 'debug_neg_noda',
+    'continuous_blending' : False,
+    'initial_blending' : True
+}
 
-# rp.ud = json.dumps(ud)
-# rp.dap = json.dumps(dap)
-# rp.queue_run()
+dap = {
+    'da_times' : [],
+}
+rp.ud = json.dumps(ud)
+rp.dap = json.dumps(dap)
+rp.queue_run()
+
+ud = {
+    'aux' : '',
+    'continuous_blending' : False,
+    'initial_blending' : True
+}
+
+dap = {
+    'none' : None,
+}
+rp.ud = json.dumps(ud)
+rp.dap = json.dumps(dap)
+rp.queue_run()
+
+ud = {
+    'aux' : '',
+    'continuous_blending' : True,
+    'initial_blending' : True
+}
+
+dap = {
+    'none' : None,
+}
+
+rp.ud = json.dumps(ud)
+rp.dap = json.dumps(dap)
+rp.queue_run()
+
 
 ##########################################
 #
-# TV queue
+# TV queue, obs RMSEs
 #
 ##########################################
 
 # rp.N = 10
 # rp.tc = 'tv'
 # ud = {
-#     'aux' : 'debug_letkf_s10p',
-#     'continuous_blending' : True
+#     'aux' : 'debug_pos_s10p_n15p',
+#     'continuous_blending' : False
 # }
 
 # dap = {
-#     # 'noise_percentage' : 0.1,
-#     'None' : None
+#     'noise_percentage' : 0.15,
 # }
 
 # rp.ud = json.dumps(ud)
@@ -41,11 +74,72 @@ rp = rp()
 # rp.queue_run()
 
 # ud = {
-#     'aux' : 'debug_letkf_s10p',
+#     'aux' : 'debug_pos_s10p_n20p',
 #     'continuous_blending' : False
 # }
 
+# dap = {
+#     'noise_percentage' : 0.20,
+# }
+
 # rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
+
+
+# ud = {
+#     'aux' : 'debug_pos_s10p_n30p',
+#     'continuous_blending' : False
+# }
+
+# dap = {
+#     'noise_percentage' : 0.30,
+# }
+
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
+
+
+# ud = {
+#     'aux' : 'debug_pos_s10p_n35p',
+#     'continuous_blending' : False
+# }
+
+# dap = {
+#     'noise_percentage' : 0.35,
+# }
+
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
+
+
+# ud = {
+#     'aux' : 'debug_pos_s10p_n40p',
+#     'continuous_blending' : False
+# }
+
+# dap = {
+#     'noise_percentage' : 0.40,
+# }
+
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
+
+
+# ud = {
+#     'aux' : 'debug_pos_s10p_n45p',
+#     'continuous_blending' : False
+# }
+
+# dap = {
+#     'noise_percentage' : 0.45,
+# }
+
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
 # rp.queue_run()
 
 
@@ -56,71 +150,71 @@ rp = rp()
 #
 ##########################################
 
-rp.N = 10
-rp.tc = 'swe_bal_vortex'
+# rp.N = 10
+# rp.tc = 'swe_bal_vortex'
 
 
-ud = {
-    'aux' : 'debug_letkf_s05p',
-    'continuous_blending' : True
-}
+# ud = {
+#     'aux' : 'debug_letkf_s05p',
+#     'continuous_blending' : True
+# }
 
-dap = {
-    'obs_attrs' : ['rhou', 'rhow'],
-    'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
-    'obs_frac' : 0.05
-}
+# dap = {
+#     'obs_attrs' : ['rhou', 'rhow'],
+#     'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
+#     'obs_frac' : 0.05
+# }
 
-rp.ud = json.dumps(ud)
-rp.dap = json.dumps(dap)
-rp.queue_run()
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
 
-rp.tc = 'swe_bal_vortex'
+# rp.tc = 'swe_bal_vortex'
 
-ud = {
-    'aux' : 'debug_letkf_s05p',
-    'continuous_blending' : False
-}
+# ud = {
+#     'aux' : 'debug_letkf_s05p',
+#     'continuous_blending' : False
+# }
 
-dap = {
-    'obs_attrs' : ['rhou', 'rhow'],
-    'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
-    'obs_frac' : 0.05
-}
+# dap = {
+#     'obs_attrs' : ['rhou', 'rhow'],
+#     'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
+#     'obs_frac' : 0.05
+# }
 
-rp.ud = json.dumps(ud)
-rp.dap = json.dumps(dap)
-rp.queue_run()
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
 
-ud = {
-    'aux' : 'debug_letkf1_s025p',
-    'continuous_blending' : True
-}
+# ud = {
+#     'aux' : 'debug_letkf1_s025p',
+#     'continuous_blending' : True
+# }
 
-dap = {
-    'obs_attrs' : ['rhou', 'rhow'],
-    'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
-    'obs_frac' : 0.025
-}
+# dap = {
+#     'obs_attrs' : ['rhou', 'rhow'],
+#     'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
+#     'obs_frac' : 0.025
+# }
 
-rp.ud = json.dumps(ud)
-rp.dap = json.dumps(dap)
-rp.queue_run()
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
 
-ud = {
-    'aux' : 'debug_letkf1_s25p',
-    'continuous_blending' : False
-}
+# ud = {
+#     'aux' : 'debug_letkf1_s25p',
+#     'continuous_blending' : False
+# }
 
-dap = {
-    'obs_attrs' : ['rhou', 'rhow'],
-    'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
-    'obs_frac' : 0.025
-}
+# dap = {
+#     'obs_attrs' : ['rhou', 'rhow'],
+#     'obs_path' : './output_swe_vortex/output_swe_vortex_ensemble=1_64_1_64_3.0_comp_1.0_pp_tra_truth_ip.h5',
+#     'obs_frac' : 0.025
+# }
 
-rp.ud = json.dumps(ud)
-rp.dap = json.dumps(dap)
-rp.queue_run()
+# rp.ud = json.dumps(ud)
+# rp.dap = json.dumps(dap)
+# rp.queue_run()
 
 
 ##########################################
