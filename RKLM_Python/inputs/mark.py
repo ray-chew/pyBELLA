5569995e39d093d93a38e90e563aefe24acc4dfc
Ray Chew <ray.chew@fu-berlin.de>
2021-10-07 12:08:53 +0200

    new lamb wave IC: Something is weird and the IC still doesn't work
    Giving up for now.

--- RKLM_Python/inputs/mark.py
+++ RKLM_Python/inputs/mark.py
@@ -31,8 +31,6 @@ class UserData(object):
     t_ref = 100.0               # [s]
     u_ref = h_ref / t_ref
 
-    Hrho = R_gas * T_ref / grav
-
     i_gravity = np.zeros((3))
     i_coriolis = np.zeros((3))
 
@@ -48,10 +46,12 @@ class UserData(object):
         self.gamm = self.gamma
         self.Rg_over_Rv = self.R_gas / self.R_vap
         self.Q = self.Q_vap / (self.R_gas * self.T_ref)
+        self.Rg = self.R_gas / (self.h_ref**2 / self.t_ref**2 / self.T_ref)
+        self.cp_gas = self.cp_gas
 
         self.nspec = self.NSPEC
 
-        self.is_nonhydrostatic = 0
+        self.is_nonhydrostatic = 1
         self.is_compressible = 1
         self.is_ArakawaKonor = 0
 
@@ -64,8 +64,7 @@ class UserData(object):
         self.coriolis_strength = np.zeros((3))
 
         self.gravity_strength[1] = self.grav * self.h_ref / (self.R_gas * self.T_ref)
-        self.coriolis_strength[0] = self.omega * self.t_ref
-        self.coriolis_strength[2] = self.omega * self.t_ref
+        self.coriolis_strength[1] = self.omega * self.t_ref
 
         for i in range(3):
             if (self.gravity_strength[i] > np.finfo(np.float).eps) or (i == 1):
@@ -78,16 +77,16 @@ class UserData(object):
         # self.xmin = - 24.0e6 / self.h_ref
         # self.xmax =   24.0e6 / self.h_ref
         j = 2.0
-        Lx = 1.0 * np.pi * self.Cs / self.N_ref * self.t_ref * j
+        Lx = 1.0 * np.pi * self.Cs / self.N_ref * j
         self.xmin = - Lx / self.h_ref
         self.xmax =   Lx / self.h_ref
         self.ymin = - 0.0
-        self.ymax =   4.0 * 8776 / self.h_ref
+        self.ymax =   4.0 * 8776.0 / self.h_ref
         # self.ymax =   1.0
         self.zmin = - 1.0
         self.zmax =   1.0
 
-        self.u_wind_speed = 20.0 * self.u_ref
+        self.u_wind_speed = 0.0 * self.u_ref
         self.v_wind_speed = 0.0
         self.w_wind_speed = 0.0
 
@@ -106,7 +105,7 @@ class UserData(object):
         self.iny = 10+1
         self.inz = 1
 
-        self.dtfixed0 = 0.5 * ((self.xmax - self.xmin) / (self.inx-1)) / 100.0
+        self.dtfixed0 = 0.5 * ((self.xmax - self.xmin) / (self.inx-1)) / 1.0
         self.dtfixed = self.dtfixed0
 
         self.limiter_type_scalars = LimiterType.NONE
@@ -162,52 +161,72 @@ def sol_init(Sol, mpv, elem, node, th, ud, seeds=None):
         # eqn (11)
         tmp = np.zeros_like(xi)
         tmp[np.where(np.abs(xi) < 1.0)] = np.exp(-1.0 / (1.0 - xi[np.where(np.abs(xi) < 1.0)]**2))
-        return tmp
-        # return np.ones_like(xi)
+        # return tmp
+        return np.ones_like(xi)
 
     hydrostatic_state(mpv, elem, node, th, ud)
     n = 4.0           # eqn (12)
     A0 = 1.0e-3     # eqn (12)
+
+    # ud.Msq = np.sqrt(ud.Msq)
+
     Msq = ud.Msq
     g = ud.gravity_strength[1]
     kappa = th.Gamma
-    Omega = ud.coriolis_strength[0]
+    Omega = ud.coriolis_strength[1]
 
-    waveno = n * 2 * np.pi / (0.5 * (ud.xmax - ud.xmin))
-    # waveno = 1.0
+    waveno = n * 2.0 * np.pi / (0.5 * (ud.xmax - ud.xmin))
 
     x = elem.x.reshape(-1,1)
     y = elem.y.reshape(1,-1)
 
-    # y-extent = n*Hrho, dimensionless.
-    Hrho = (ud.ymax - ud.ymin) / n                # Hrho = R*bar{T}/g
-    kGam = (1.0 - 0.5 * th.gamm) / Hrho                # eqn (8)
-    Htheta = Hrho / kappa
+
+    Hrho = 1.0 / g
+    kGam = (1.0 - th.gamm / 2.0) / Hrho                 # eqn (8)
     A = A0 * bump(2.0 * y / (n * Hrho) - 1)             # eqn (12)
+
+    use_hydrostate = False
+    
+    if use_hydrostate:
+        # Use hydrostatically balanaced background
+        rhobar = mpv.HydroState.rho0.reshape(1,-1)
+        Ybar = mpv.HydroState.Y0.reshape(1,-1)
+        pibar = mpv.HydroState.p20.reshape(1,-1)
+        div = 1.0
+    else:
+        # Use hydrostatic balance in Mark's notes
+        Htheta = Hrho / kappa
+        rhobar = np.exp(-y / Hrho) # eqn (7)
+        Ybar = np.exp(y / Htheta)  # eqn (9)
+        pibar = 1.0 / Ybar
+        div = Msq
 
     N = ud.t_ref * np.sqrt(ud.Nsq_ref)                  # eqn (5) dimensionless Brunt-Väisälä frequency
     Cs = np.sqrt(th.gamm / Msq)                         # eqn (6) dimensionless speed of sound
 
-    rhobar = np.exp(-y / Hrho)                          # eqn (7)
-    Ybar = np.exp(y / Htheta)                           # eqn (9)
-    rhobar = mpv.HydroState.rho0.reshape(1,-1)
-    Ybar = mpv.HydroState.Y0.reshape(1,-1)
+    u0 = ud.u_wind_speed
+    v0 = ud.v_wind_speed
+    w0 = ud.w_wind_speed
 
-    pibar = mpv.HydroState.p20.reshape(1,-1)            # background pi
-    # pibar = np.exp(- y / Htheta)
+    # 1.0 / c_p in the notes
+    # fac = 1.0 / (ud.cp_gas / (ud.h_ref**2 / ud.t_ref**2 / ud.T_ref))
+    fac = th.Gamma
 
-    # pibar = 0.0
 
+    # set up perturbation quantities
     # exp(-kGam * y)  / sqrt(rhobar) / Ybar = 1.0
-    u = A * np.sqrt(1.0 / rhobar) * np.exp(-kGam * y) * np.cos(N / Cs * waveno * x)
-    v = -A * np.sqrt(Omega / (Cs * kGam)) * np.sqrt(1.0 / rhobar) * np.exp(-kGam * y) * np.sin(N / Cs * waveno * x)
-    w = 0.0
+    up = A * np.sqrt(1.0 / rhobar) * np.exp(-kGam * y) * np.cos(N / Cs * waveno * x)
+    vp = -A * np.sqrt(Omega / (Cs * kGam)) * np.sqrt(1.0 / rhobar) * np.exp(-kGam * y) * np.sin(N / Cs * waveno * x)
+    wp = 0.0
     Yp = A * np.sqrt(Omega / (Cs * kGam)) * N / g * Ybar * np.sqrt(1.0 / rhobar) * np.exp(-kGam * y) * np.sin(N / Cs * waveno * x)     # eqn (3)
-    Y = Ybar + Yp
-    pi = pibar * Msq + A * Cs * th.Gamma * np.cos(N / Cs * waveno * x)     # eqn (4)
+    pi = A * Cs * fac * np.sqrt(1.0 / rhobar) / Ybar * np.exp(-kGam * y) * np.cos(N / Cs * waveno * x)     # eqn (4)
 
-    # p = pi**(th.Gamminv)
-    rho = pi**th.gm1inv / Y
+    u = u0 + up
+    v = v0 + vp
+    w = w0 + wp
+    Y = Ybar + Yp
+
+    rho = ((pibar * Msq / div + pi)**th.gm1inv) / Y
 
     Sol.rho[...] = rho
     Sol.rhou[...] = rho * u
@@ -215,20 +234,33 @@ def sol_init(Sol, mpv, elem, node, th, ud, seeds=None):
     Sol.rhow[...] = rho * w
     Sol.rhoe[...] = 0.0
     Sol.rhoY[...] = rho * Y
-    # Sol.rhoX[...] = rho / (Sol.rho / Sol.rhoY - mpv.HydroState.S0.reshape(1,-1))
-    Sol.rhoX[...] = rho / Y
-    mpv.HydroState.S0[...] *= 0.0
+    Sol.rhoX[...] = 0.0
     mpv.p2_cells[...] = pi / Msq
 
+    ###################################################
     # initialise nodal pi
     xn = node.x.reshape(-1,1)
     yn = node.y.reshape(1,-1)
 
-    An = A0 * bump(2.0 * yn / (n * Hrho) - 1.0)
-    pibar_n = mpv.HydroState_n.p20.reshape(1,-1)
-    # pibar_n = 0.0
+    # initialise nodal pressure
+    Hrho_n = Hrho
+    kGam_n = (1.0 - th.gamm / 2.0) / Hrho_n
 
-    pi_n = pibar_n * Msq + An * Cs * th.Gamma * np.cos(N / Cs * waveno * xn)
+    if use_hydrostate:
+        # Use hydrostatically balanced background
+        rhobar_n = mpv.HydroState_n.rho0.reshape(1,-1)
+        Ybar_n = mpv.HydroState_n.Y0.reshape(1,-1)
+        pibar_n = mpv.HydroState_n.p20.reshape(1,-1)
+    else:
+        # Use hydrostatic balance from notes
+        rhobar_n = np.exp(-yn / Hrho_n)
+        Ybar_n = np.exp(-yn / Htheta)
+        pibar_n = 1.0 / Ybar_n
+
+    An = A0 * bump(2.0 * yn / (n * Hrho_n) - 1.0)
+
+    pi_n = An * Cs * fac * np.sqrt(1.0 / rhobar_n) / Ybar_n * np.exp(-kGam_n * yn) * np.cos(N / Cs * waveno * xn)
+
     mpv.p2_nodes[...] = pi_n / Msq
 
     set_explicit_boundary_data(Sol,elem,ud,th,mpv)
