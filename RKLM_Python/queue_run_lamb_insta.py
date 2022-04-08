c010d2166f98359cff5731a498f026d321c33667
Ray Chew <ray.chew@fu-berlin.de>
2022-04-08 03:47:30 +0200

    here comes the queue file

---
+++ RKLM_Python/queue_run_lamb_insta.py
@@ -0,0 +1,44 @@
+from run import run_params as rp
+import json
+import numpy as np
+
+rp = rp()
+path_to_obs = './'
+
+rp.N = 1
+
+rp.tc = 'mark'
+
+t_ref = 100.0
+omega = 7.292 * 1e-5
+resol_x = [151,301,601,1201]
+resol_y = [15,30,60,120]
+resol_t = [200,400,600,800,1000,1200]
+omegas = [0.0, 2.0 * omega * t_ref]
+
+
+ud = {}
+dap = {
+    'None' : None,
+}
+
+for x,y in zip(resol_x,resol_y):
+    ud['inx'] = x+1
+    ud['iny'] = y+1
+
+    for t in resol_t:
+        ud['dtfixed0'] = t / t_ref
+        ud['dtfixed']  = t / t_ref
+
+        for om in omegas:
+            ud['coriolis_strength'] = [0.0, 0.0, om]
+            if om > 0:
+                ud['aux'] = 'bdl_run_S%i_a05' %t
+            else:
+                ud['aux'] = 'bdl_run_S%i_noom_a05' %t
+
+            print(ud)
+            # # run simulation
+            rp.ud = json.dumps(ud)
+            rp.dap = json.dumps(dap)
+            rp.queue_run()
