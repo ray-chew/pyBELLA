6031362ff7182ff6df77f1c860a6726a3a5966f7
raychew <chew@iau.uni-frankfurt.de>
2023-11-23 13:56:14 +0100

    commit minor changes to input files before commencing test suite implementation

--- RKLM_Python/inputs/lamb_waves.py
+++ RKLM_Python/inputs/lamb_waves.py
@@ -1,7 +1,7 @@
 import numpy as np
 from inputs.enum_bdry import BdryType
 from management.enumerator import TimeIntegrator, MolecularTransport,HillShapes, BottomBC, LimiterType, RecoveryOrder
-from physics.hydrostatics import hydrostatic_state, hydrostatic_column, hydrostatic_initial_pressure
+from physics.hydrostatics import hydrostatic_state
 from inputs.boundary import set_explicit_boundary_data, set_ghostcells_p2, set_ghostnodes_p2
 from management.variable import States
 
