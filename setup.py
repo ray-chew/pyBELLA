3b1351b8d2cead1f82565e64e2ee2dec338dc961
raychew <chew@iau.uni-frankfurt.de>
2023-12-06 12:16:17 +0100

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name='RKLM', version='0.5', packages=find_packages())
+setup(name='RKLM', 
+      version='0.5', 
+      packages=find_packages(),
+      install_requires=[
+        'numba',
+        'numpy==1.22.1',
+        'h5py',
+        'scipy',
+        'PyYAML',
+        'termcolor',
+        'dask',
+        'dask[distributed]',
+        'matplotlib',
+        'dill',
+      ]
+      )
